3ea010a0ce0de60d0f4eb0e98779782399c64413
zlin <2768228421@qq.com>
2024-10-15 16:27:19 +0800

    10,15

--- _CF/GYM/The 2023 ICPC Asia Hangzhou Regional Contest/G.cpp
+++ _CF/GYM/The 2023 ICPC Asia Hangzhou Regional Contest/G.cpp
@@ -15,9 +15,10 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-int cnt[3010][3010];
-int idx[3010][3010];
-char Map[3010][3010];
+int cnt[3010][3010]{};
+int idx[3010][3010]{};
+int vis[3010][3010]{};
+char Map[3010][3010]{};
 int n, m, k, fx, fy;
 
 struct node {
@@ -39,23 +40,31 @@ inline void Zlin() {
         cnt[x][y] = i;
     }
     for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++) {
-            cin >> Map[i][j];
-            idx[i][j] = 1e7;
-        }
+        for (int j = 1; j <= m; j++)
+            cin >> Map[i][j], idx[i][j] = 1e7;
     priority_queue<node> q;
     q.push({fx, fy, k});
+    idx[fx][fy] = k;
     while (!q.empty()) {
         node u = q.top();
         q.pop();
-        if (idx[u.x][u.y] <= u.idx) continue;
-        idx[u.x][u.y] = u.idx;
-        for (int i = 0; i < 4; i++) {
-            int vx = u.x + dx[i], vy = u.y + dy[i];
-            if (vx < 1 || vx > n || vy < 1 || vy > m) continue;
-
+        if (!cnt[u.x][u.y]) cnt[u.x][u.y] = u.idx;
+        for (int i = 0, vx, vy, dif; i < 4; i++) {
+            vx = u.x + dx[i], vy = u.y + dy[i];
+            if (vx < 1 || vx > n || vy < 1 || vy > m || Map[vx][vy] == '#') continue;
+            dif = max(k - 1 + cnt[vx][vy] - idx[u.x][u.y], 0) + u.idx + 1;
+            if (dif < idx[vx][vy]) {
+                idx[vx][vy] = dif;
+                q.push({vx, vy, dif});
+            }
         }
     }
+    ull ans = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            if (idx[i][j] != 1e7)
+                ans += 1ll * (idx[i][j] - k) * (idx[i][j] - k);
+    cout << ans << '\n';
 }
 
 int main() {
