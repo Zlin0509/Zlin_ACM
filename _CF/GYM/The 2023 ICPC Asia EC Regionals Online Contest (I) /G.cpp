f2745a8223b2fbca39f991b4c3ebd6d0ad87bd36
zlin <2768228421@qq.com>
2024-10-18 21:34:17 +0800

    10,18

--- _CF/GYM/The 2023 ICPC Asia EC Regionals Online Contest (I) /G.cpp
+++ _CF/GYM/The 2023 ICPC Asia EC Regionals Online Contest (I) /G.cpp
@@ -17,15 +17,44 @@ typedef pair<long long, long long> pll;
 
 const ll mo = 998244353;
 
-inline void Zlin() {
+ll qpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % mo;
+        a = a * a % mo;
+        b >>= 1;
+    }
+    return res;
+}
 
+ll inv(ll a) {
+    return qpow(a, mo - 2);
+}
+
+const int N = 1e6 + 5;
+int n;
+int o1[N], o2[N];
+map<pii, int> e;
+ll x = 1, y = 1;
+
+inline void Zlin() {
+    cin >> n;
+    for (int i = 1; i < n; i++) cin >> o1[i] >> o2[i];
+    for (int i = 1, u, v; i < n; i++) {
+        cin >> u >> v;
+        e[{u, v}] = e[{v, u}] = 1;
+    }
+    for (int i = 1; i < n; i++) {
+
+    }
+    cout << x * inv(y) % mo;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
-    cin >> ttt;
+//    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
