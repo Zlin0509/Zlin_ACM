14f4ab6019c95533f4092fba163deb7b9611510d
zlin <2768228421@qq.com>
2024-10-23 16:35:08 +0800

    10，23

--- _CF/CONTEST/2013/2013E.cpp
+++ _CF/CONTEST/2013/2013E.cpp
@@ -16,39 +16,29 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 const int N = 1e5 + 5;
-int n, a[N], dp[30][30], cc[30][30];
+int n, a[N];
 
 inline void Zlin() {
     cin >> n;
     for (int i = 1; i <= n; i++) cin >> a[i];
     sort(a + 1, a + 1 + n);
     int tag = a[1];
-    vi used;
-    used.push_back(a[1]);
-    for (int i = 2; i <= n; i++) {
-        if (gcd(tag, a[i]) < tag) {
-            tag = gcd(tag, a[i]);
-            used.push_back(a[i]);
-        }
+    for (int i = 1; i <= n; i++) tag = gcd(tag, a[i]);
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) a[i] /= tag;
+    int now = a[1], cur, cnt = n;
+    ans += now * tag;
+    --cnt;
+    while (now != 1) {
+        cur = now;
+        for (int i = 1; i <= n; i++)
+            cur = min(cur, gcd(now, a[i]));
+        now = cur;
+        ans += now * tag;
+        --cnt;
     }
-    ll ans = 1ll * tag * (n - used.size());
-    int len = used.size();
-    for (int i = 0; i < len; i++) dp[1][i] = cc[1][i] = used[i];
-    for (int i = 2; i <= len; i++) {
-        for (int j = 0; j < len; j++) {
-            dp[i][j] = 1e9;
-            for (int q = 0; q < len; q++) {
-                if (dp[i][j] > gcd(cc[i - 1][q], used[j]) + dp[i - 1][q]) {
-                    cc[i][j] = gcd(cc[i - 1][q], used[j]);
-                    dp[i][j] = cc[i][j] + dp[i - 1][q];
-                }
-            }
-        }
-    }
-    tag = 1e9;
-    for (int i = 0; i < len; i++) tag = min(tag, dp[len][i]);
-//    cout << tag << " ";
-    cout << ans + tag << '\n';
+    ans += 1ll * tag * cnt;
+    cout << ans << '\n';
 }
 
 int main() {
