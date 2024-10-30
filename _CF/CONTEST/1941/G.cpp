a7355163561be83374731f627b6513283491ab5e
zlin <2768228421@qq.com>
2024-10-30 11:34:38 +0800

    10，30

--- _CF/CONTEST/1941/G.cpp
+++ _CF/CONTEST/1941/G.cpp
@@ -15,8 +15,56 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-inline void Zlin() {
+const int N = 2e5 + 5;
+const int inf = 1e9;
 
+struct edge {
+    int v, val, nxt;
+} e[N << 1];
+int head[N], tot;
+
+inline void add(int u, int v, int c) {
+    e[++tot] = {v, c, head[u]};
+    head[u] = tot;
+}
+
+int n, m, Start, End, dep[N], vis[N], c[N];
+
+inline void init() {
+    tot = 0;
+    for (int i = 1; i <= n; i++) {
+        vis[i] = head[i] = 0;
+        dep[i] = inf;
+    }
+}
+
+inline void Zlin() {
+    cin >> n >> m;
+    init();
+    for (int i = 1, u, v, val; i <= m; i++) {
+        cin >> u >> v >> val;
+        add(u, v, val);
+        add(v, u, val);
+        c[val] = inf;
+    }
+    cin >> Start >> End;
+    dep[Start] = 0;
+    priority_queue<pii> q;
+    q.push({0, Start});
+    while (!q.empty()) {
+        int u = q.top().second;
+        q.pop();
+        if (vis[u]) continue;
+        vis[u] = 1;
+        for (int i = head[u]; i; i = e[i].nxt) {
+            c[e[i].val] = min(dep[u] + 1, c[e[i].val]);
+            if (dep[e[i].v] > c[e[i].val]) {
+                dep[e[i].v] = c[e[i].val];
+                if (!vis[e[i].v]) q.push({-dep[e[i].v], e[i].v});
+            }
+        }
+    }
+    cout << dep[End] << '\n';
 }
 
 int main() {
