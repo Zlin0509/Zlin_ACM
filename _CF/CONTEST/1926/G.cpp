5ab88f9cc1e4f5218ff7d6567b225fa41cb58a48
zlin <2768228421@qq.com>
2024-10-30 13:07:33 +0800

    10，30

--- _CF/CONTEST/1926/G.cpp
+++ _CF/CONTEST/1926/G.cpp
@@ -15,8 +15,48 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-inline void Zlin() {
+const int N = 1e5 + 5;
+const int inf = 1e9;
 
+struct edge {
+    int v, nxt;
+} e[N << 1];
+int head[N], tot;
+
+inline void add(int u, int v) {
+    e[++tot] = {v, head[u]};
+    head[u] = tot;
+}
+
+int n, f[N][2];
+string s;
+
+inline void dfs(int u, int fa) {
+    f[u][0] = f[u][1] = 0;
+    for (int i = head[u]; i; i = e[i].nxt) {
+        int v = e[i].v;
+        if (v == fa) continue;
+        dfs(v, u);
+        f[u][0] += min(f[v][0], f[v][1] + 1);
+        f[u][1] += min(f[v][1], f[v][0] + 1);
+    }
+    if (s[u] == 'P') f[u][0] = inf;
+    if (s[u] == 'S') f[u][1] = inf;
+}
+
+inline void Zlin() {
+    cin >> n;
+    tot = 0;
+    for (int i = 1; i <= n; i++) head[i] = 0;
+    for (int i = 2, v; i <= n; i++) {
+        cin >> v;
+        add(i, v);
+        add(v, i);
+    }
+    cin >> s;
+    s = ' ' + s;
+    dfs(1, 0);
+    cout << min(f[1][0], f[1][1]) << '\n';
 }
 
 int main() {
