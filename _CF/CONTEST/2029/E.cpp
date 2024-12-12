e8840f78067deb8c2bdea23538b0aeda55626420
zlin <2768228421@qq.com>
2024-12-12 12:15:51 +0800

    00

--- _CF/CONTEST/2029/E.cpp
+++ _CF/CONTEST/2029/E.cpp
@@ -16,53 +16,59 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 const int N = 1e5 + 5;
-int n;
-unordered_map<int, bool> have;
-vi a, s{2, 3, 5, 6, 7};
+int n, a[N];
 
-bool check(int x) {
-    if (x == 2) {
-        if (have[3] || have[5] || have[7]) return false;
-    } else if (x == 3) {
-        if (have[4] || have[5] || have[7]) return false;
-    } else if (x == 5) {
-        if (have[6] || have[7] || have[8] || have[9] || have[11] || have[13]) return false;
-    } else if (x == 6) {
-        if (have[7]) return false;
-    } else if (x == 7) {
-        for (int i = 8; i <= 13; i++) if (have[i]) return false;
-        if (have[15] || have[17] || have[19]) return false;
+const int NN = 4e5 + 200;
+int tag[NN];
+
+void dd() {
+    tag[1] = 1;
+    vi primes;
+    for (int i = 2; i < NN; i++) {
+        if (!tag[i]) primes.push_back(i);
+        for (int j = 0; j < primes.size() && primes[j]*i < NN; j++) {
+            tag[i * primes[j]] = primes[j];
+            if (i % primes[j] == 0) break;
+        }
     }
-    return true;
 }
 
 inline void Zlin() {
     cin >> n;
-    a.clear();
-    have.clear();
-    for (int i = 1, x; i <= n; i++) {
-        cin >> x;
-        if (x <= 7) a.push_back(x);
-        if (x <= 20) have[x] = true;
-    }
-    sort(a.begin(), a.end());
-    int ans = -1;
-    if (!a.empty()) {
-        for (auto i: s) {
-            if (i > *a.begin()) break;
-            if (check(i)) {
-                ans = i;
-                break;
-            }
+    int x = 0;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) {
+        if (tag[a[i]] || a[i] == 2) continue;
+        if (!x) x = a[i];
+        else {
+            cout << -1 << '\n';
+            return;
         }
-    } else ans = 2;
-    cout << ans << '\n';
+    }
+    if (!x) {
+        cout << 2 << '\n';
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) continue;
+        if ((a[i] & 1) && a[i] - tag[a[i]] < 2 * x) {
+            cout << -1 << '\n';
+            return;
+        }
+        if (!(a[i] & 1) && a[i] < 2 * x) {
+            cout << -1 << '\n';
+            return;
+        }
+    }
+    cout << x << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
+    dd();
+//    for (int i = 1; i <= 100; i++) cout << tag[i] << ' ';
     cin >> ttt;
     while (ttt--) Zlin();
     return 0;
