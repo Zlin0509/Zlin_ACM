22114eb12b8592a45204d47cd49190d32da2a8f3
zlin <2768228421@qq.com>
2024-12-15 20:51:36 +0800

    00

--- Game/A.cpp
+++ Game/A.cpp
@@ -15,38 +15,70 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-const int N = 1e6 + 10;
+const int N = 2e5 + 20;
 
+int n, m;
+vector<vi> a;
+
+int l[N], r[N];
+
+vi in[N], out[N];
+
+struct node {
+    int val, id;
+
+    bool operator<(const node &x) const {
+        return val > x.val;
+    }
+};
 
 inline void Zlin() {
-    int n, m;
     cin >> n >> m;
-    int cnt = 0;
-    vi a(N), ans(N), b(N), c(N);
-    unordered_map<int, int> used, num;
-    for (int i = 1, x; i <= n; i++) {
-        cin >> b[i];
-        if (!used[b[i]]) {
-            used[b[i]] = 1;
-            a[cnt++] = b[i];
+    a.assign(n + 1, vi(m + 1, 0));
+    for (int i = 1; i <= m; i++) l[i] = 1, r[i] = N;
+    vi have;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+            if (a[i][j] != -1) have.push_back(a[i][j]);
         }
-        ++num[b[i]];
+        sort(have.begin(), have.end());
+        have.erase(unique(have.begin(), have.end()), have.end());
+        for (int j = 1; j <= m; j++)
+            if (a[i][j] != -1) {
+                int x = lower_bound(have.begin(), have.end(), a[i][j]) - have.begin() + 1;
+                l[j] = max(l[j], x);
+                r[j] = min(r[j], m - (int) have.size() + x);
+            }
+        have.clear();
     }
-    for (int i = 0; i < cnt; i++) c[i] = num[a[i]];
-    for (int i = 0; i < cnt; i++)
-        for (int j = 1; a[i] * j <= m; j++)
-            ans[a[i] * j] += c[i];
-//    for (int i = 1; i <= m; i++) cout << ans[i] << ' ';
-    int res = 0;
-    for (int i = 1; i <= m; i++)
-        if (ans[res] < ans[i])
-            res = i;
-    if (!ans[res]) {
-        cout << "1 0" << '\n';
+    for (int j = 1; j <= m; j++) {
+        in[l[j]].push_back(j);
+        out[r[j]].push_back(j);
+    }
+    vi ans;
+    set<int> now;
+    priority_queue<node> q;
+    for (int i = 1; i <= m; i++) {
+        for (auto id: in[i]) q.push({r[id], id}), now.insert(id);
+        if (q.empty()) {
+            cout << -1 << '\n';
+            return;
+        }
+        ans.push_back(q.top().id);
+        now.erase(now.find(q.top().id));
+        q.pop();
+        for (auto id: out[i])
+            if (now.find(id) != now.end()) {
+                cout << -1 << '\n';
+                return;
+            }
+    }
+    if (!q.empty()) {
+        cout << -1 << '\n';
         return;
     }
-    cout << res << ' ' << ans[res] << '\n';
-    for (int i = 1; i <= n; i++) if (res % b[i] == 0) cout << i << ' ';
+    for (auto it: ans) cout << it << ' ';
 }
 
 int main() {
