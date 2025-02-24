b6a6cf898dcb12ec4ad270672ec63ddb4b73279a
27682 <2768228421@qq.com>
2025-02-24 16:40:10 +0800

    2.24

--- _CF/CONTEST/2045/B.cpp
+++ _CF/CONTEST/2045/B.cpp
@@ -4,7 +4,7 @@
 
 #include "bits/stdc++.h"
 
-#define endl  '\n'
+#define endl '\n'
 using namespace std;
 
 typedef double db;
@@ -17,25 +17,15 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 inline void Zlin() {
-    int n, m;
-    cin >> n >> m;
-    if (m >= n * 2) {
-        cout << "NO" << endl;
-        return;
-    }
-    cout << "YES" << endl;
-    for (int i = 1; i <= n * 2; i++) {
-        for (int j = 1; j <= m; j++)
-            cout << (i + j) % (2 * n) / 2 + 1 << ' ';
-        cout << endl;
-    }
+    ll n, d, s;
+    cin >> n >> d >> s;
+    ll ans = s;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
-    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
