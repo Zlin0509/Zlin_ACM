7ecd0f2d5fc1c31098a1fe429fbe8c562bce1976
zlin <2768228421@qq.com>
2025-02-25 16:13:58 +0800

    2.25

--- _CF/CONTEST/2045/B.cpp
+++ _CF/CONTEST/2045/B.cpp
@@ -16,13 +16,38 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-inline void Zlin() {
+inline void Zlin()
+{
     ll n, d, s;
     cin >> n >> d >> s;
-    ll ans = s;
+    n /= s, d /= s;
+    if (d == 0)
+    {
+        cout << s << endl;
+        return;
+    }
+    if (n <= d + 1)
+    {
+        cout << n * s << endl;
+        return;
+    }
+    n = min(n, d * 2ll);
+    // if (n % 2 == 0)
+    // {
+    //     cout << n * s << endl;
+    //     return;
+    // }
+    for (ll i = 2; i <= 1e6; i++)
+        if (n % i == 0 && n - n / i <= d)
+        {
+            cout << n * s << endl;
+            return;
+        }
+    cout << (n - 1) * s << endl;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
