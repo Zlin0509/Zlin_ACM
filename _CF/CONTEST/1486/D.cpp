b450718f15ecf55a9a0567d8edd848e0ade286a5
Zlin_PC <zlin0509@outlook.com>
2025-02-27 11:23:50 +0800

    2.27

--- _CF/CONTEST/1486/D.cpp
+++ _CF/CONTEST/1486/D.cpp
@@ -5,6 +5,7 @@
 #include "bits/stdc++.h"
 
 #define endl '\n'
+#define lowbit(x) (x&(-x))
 using namespace std;
 
 typedef vector<int> vi;
@@ -15,13 +16,37 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 inline void Zlin() {
+    int n, k;
+    cin >> n >> k;
+    vi a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    auto check = [&](int x) {
+        vi sum(n + 1), mm(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + (a[i] < x ? 1 : -1);
+            mm[i] = max(mm[i - 1], sum[i]);
+        }
+        for (int i = k; i <= n; i++)
+            if (sum[i] - mm[i - k] < 0)
+                return true;
+        return false;
+    };
+    int l = 1, r = n, mid;
+    while (l < r) {
+        mid = l + r + 1 >> 1;
+        if (check(mid))
+            l = mid;
+        else
+            r = mid - 1;
+    }
+    cout << l << endl;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     int ttt = 1;
-    cin >> ttt;
     while (ttt--)
         Zlin();
     return 0;
