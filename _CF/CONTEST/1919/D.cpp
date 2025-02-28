9dfde2eb03326f89f4038df9a46218ea5fcdcc1f
Zlin_PC <zlin0509@outlook.com>
2025-02-28 16:56:55 +0800

    2.27

--- _CF/CONTEST/1919/D.cpp
+++ _CF/CONTEST/1919/D.cpp
@@ -16,6 +16,38 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 inline void Zlin() {
+    int n;
+    cin >> n;
+    vi a(n);
+    int cnt = 0;
+    for (int &it: a)
+        cin >> it, cnt += it == 0;
+    if (cnt != 1) {
+        cout << "NO" << endl;
+        return;
+    }
+    vector aa(n + 1, vi());
+    for (int i = 0; i < n; i++)
+        aa[a[i]].push_back(i);
+    set<int> have;
+    have.insert(aa[0][0]);
+    for (int i = 1; i <= n; i++) {
+        for (int id: aa[i]) {
+            bool check = false;
+            auto it = have.lower_bound(id);
+            if (it != have.end() && a[*it] == a[id] - 1)
+                check = true;
+            if (it != have.begin() && a[*prev(it)] == a[id] - 1)
+                check = true;
+            if (!check) {
+                cout << "NO" << endl;
+                return;
+            }
+        }
+        for (int id: aa[i])
+            have.insert(id);
+    }
+    cout << "YES" << endl;
 }
 
 int main() {
