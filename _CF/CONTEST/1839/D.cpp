0ae519e271948f38207a31fb69ff417b1329136e
Zlin_PC <zlin0509@outlook.com>
2025-03-02 14:51:09 +0800

    2.28

--- _CF/CONTEST/1839/D.cpp
+++ _CF/CONTEST/1839/D.cpp
@@ -1,29 +1,50 @@
-//
-// Created by 27682 on 2025/2/28.
-//
+// LUOGU_RID: 112012510
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
 
-#include "bits/stdc++.h"
+int T, n;
+#define N 510
+int a[N];
+int dp[N][N];
+bool f[N][N];
 
-#define endl '\n'
-using namespace std;
-
-typedef long long ll;
-typedef double db;
-typedef long double ldb;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-
-inline void Zlin() {
-}
+void ckmin(int &x, int y) { x = x < y ? x : y; }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    int ttt = 1;
-    cin >> ttt;
-    while (ttt--)
-        Zlin();
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d", &n);
+        for (int i = 1; i <= n; i++)
+            scanf("%d", &a[i]);
+        for (int l = 1; l <= n; l++) {
+            f[l][l] = 1;
+            for (int r = l + 1; r <= n; r++)
+                f[l][r] = f[l][r - 1] & (a[r - 1] < a[r]);
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = f[1][i] ? 0 : 0x3f3f3f3f;
+        for (int k = 1; k <= n; k++) {
+            dp[0][k] = 0;
+            for (int i = 1; i <= n; i++) {
+                dp[i][k] = dp[i][k - 1];
+                if (a[i - 1] < a[i])
+                    ckmin(dp[i][k], dp[i - 1][k]);
+                for (int j = 0; j < i - 1; j++)
+                    if (a[j] < a[i])
+                        ckmin(dp[i][k], dp[j][k - 1] + i - 1 - j);
+                // printf("dp[%d][%d] = %d\n", i, k, dp[i][k]);
+            }
+        }
+        for (int k = 1; k <= n; k++) {
+            int ans = dp[n][k];
+            for (int i = 1; i < n; i++)
+                ckmin(ans, dp[i][k - 1] + n - i);
+            printf("%d ", ans);
+        }
+        putchar('\n');
+    }
     return 0;
 }
