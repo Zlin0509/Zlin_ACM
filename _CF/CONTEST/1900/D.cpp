48986ec218b50cc93e1771399d4bb1b4ca07764b
zlin <2768228421@qq.com>
2025-03-02 21:36:16 +0800

    3.2

--- _CF/CONTEST/1900/D.cpp
+++ _CF/CONTEST/1900/D.cpp
@@ -15,8 +15,43 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+constexpr int N = 1e5 + 5;
+
+vector su(N, vi());
+
+inline void dd()
+{
+    for (int i = 1; i < N; i++)
+        for (int j = i; j < N; j += i)
+            su[j].push_back(i);
+    for (int i = 1; i < N; i++)
+        reverse(su[i].begin(), su[i].end());
+}
+
 inline void Zlin()
 {
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int& it : a)
+        cin >> it;
+    sort(a.begin(), a.end());
+    ll ans = 0, sum = 0;
+    vi f(N), g(N);
+    for (int it : a)
+    {
+        ans += sum;
+        for (int x : su[it])
+        {
+            f[x] = g[x];
+            for (int y : su[it / x])
+                if (y != 1)
+                    f[x] -= f[x * y];
+            sum += 1ll * x * f[x];
+            g[x]++;
+        }
+    }
+    cout << ans << endl;
 }
 
 int main()
@@ -25,6 +60,7 @@ int main()
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
+    dd();
     while (ttt--) Zlin();
     return 0;
 }
