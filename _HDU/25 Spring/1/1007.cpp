82251bb48e746d088f7fd017a40f880164f23686
Zlin_PC <zlin0509@outlook.com>
2025-03-08 11:05:09 +0800

    3.7

--- _HDU/25 Spring/1/1007.cpp
+++ _HDU/25 Spring/1/1007.cpp
@@ -14,8 +14,27 @@ typedef vector<ll> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+constexpr ll mo = 1e9 + 7;
+
+// 快速幂函数：计算 a^b % mo
+ll qpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % mo;
+        a = a * a % mo;
+        b >>= 1;
+    }
+    return res;
+}
+
 inline void Zlin()
 {
+    int n;
+    cin >> n;
+    int x = (n + 2) / 2 - 1;
+    ll ans = 0;
+    ans = (n+n+1-x)*x%mo*qpow(2,mo - 2)%mo;
+    cout << ans << endl;
 }
 
 int main()
