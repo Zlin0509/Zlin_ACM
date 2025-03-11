ca5c486d53490cfc676ba2b7991a8af7a8c3c7da
Zlin_PC <zlin0509@outlook.com>
2025-03-11 17:52:00 +0800

    3.11

--- _CF/CONTEST/1500-1999/1930/D2.cpp
+++ _CF/CONTEST/1500-1999/1930/D2.cpp
@@ -50,7 +50,7 @@ public:
         if (l <= 1) return qry1(r);
         return qry1(r) - qry1(l - 1);
     }
-} t;
+} t[2], c[2];
 
 inline void Zlin()
 {
@@ -59,25 +59,41 @@ inline void Zlin()
     string s;
     cin >> s;
     s = ' ' + s;
+    t[0].init(n * 2 + 5);
+    t[1].init(n * 2 + 5);
+    c[0].init(n * 2 + 5);
+    c[1].init(n * 2 + 5);
     vi a(n + 1);
     int cnt = 0;
     for (int i = 1; i <= n; i++)
         a[i] = a[i - 1] + (s[i] == '1' ? 1 : -1);
-    t.init(n * 2 + 5);
     for (int i = 1; i <= n; i++)
     {
         if (s[i] == '1')
             ++cnt;
         a[i] += n + 1;
-        t.upd(a[i], 1);
+        t[i & 1].upd(a[i], (i + 1) / 2);
+        c[i & 1].upd(a[i], 1);
     }
+    a[0] += n + 1;
     ll ans = 0;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0, k; i <= n; i++)
     {
         if (!cnt)
             continue;
-        ans += t.qry2(a[i - 1], n * 2 + 5);
-        t.upd(a[i], -1);
+        k = (i + 1) / 2;
+        c[i & 1].upd(a[i], -1);
+        t[i & 1].upd(a[i], -k);
+        if (i & 1)
+        {
+            ans += t[0].qry2(a[i], n * 2 + 5) - (k - 1) * c[0].qry2(a[i], n * 2 + 5);
+            ans += t[1].qry2(a[i], n * 2 + 5) - k * c[1].qry2(a[i], n * 2 + 5);
+        }
+        else
+        {
+            ans += t[0].qry2(a[i], n * 2 + 5) - k * c[0].qry2(a[i], n * 2 + 5);
+            ans += t[1].qry2(a[i], n * 2 + 5) - k * c[1].qry2(a[i], n * 2 + 5);
+        }
         if (s[i] == '1')
             --cnt;
     }
--- check/AC.cpp
+++ check/AC.cpp
@@ -17,9 +17,22 @@ typedef pair<long long, long long> pll;
 
 inline void Zlin()
 {
-    int x;
-    cin >> x;
-    cout << x * 2 << endl;
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    s = ' ' + s;
+    ll ans = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = i; j <= n; j++)
+        {
+            int cnt = 0;
+            for (int k = i; k <= j; k++)
+                cnt += s[k] == '1';
+            if (cnt >= (i + 1) / 2)
+                ans += (i + 1) / 2;
+        }
+    cout << ans << endl;
 }
 
 int main()
