ea394216bd7245d5585d6e266af7f875aaa60ab6
27682 <2768228421@qq.com>
2025-03-12 11:30:04 +0800

    3.12

--- _CF/CONTEST/1500-1999/1924/B.cpp
+++ _CF/CONTEST/1500-1999/1924/B.cpp
@@ -1,146 +1,104 @@
-//
-// Created by 27682 on 2025/3/11.
-//
-
-#include "bits/stdc++.h"
-#define endl '\n'
+#include<bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef double db;
-typedef long double ldb;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+#define int long long
+#define MAXN 300005
 
-struct Ftree
+int n, m, q, X[MAXN], V[MAXN], opt[MAXN], qx[MAXN], qv[MAXN], ql[MAXN], qr[MAXN];
+int tr[MAXN + 5], Ans[MAXN];
+
+inline void add(int x, int k) { for (; x <= MAXN; x += x & -x) tr[x] += k; }
+
+inline int sum(int x)
 {
-private:
-    vll t;
-
-public:
-    void init(int n)
-    {
-        t.assign(n + 1, 0);
-    }
-
-    void upd(ll i, ll v)
-    {
-        while (i < t.size())
-        {
-            t[i] += v;
-            i += i & -i;
-        }
-    }
-
-    ll qry1(ll i)
-    {
-        ll s = 0;
-        while (i > 0)
-        {
-            s += t[i];
-            i -= i & -i;
-        }
-        return s;
-    }
-
-    ll qry2(ll l, ll r)
-    {
-        if (r < l)
-            return 0;
-        return qry1(r) - qry1(l - 1);
-    }
-} t;
+	int ret = 0;
+	for (; x; x -= x & -x) ret += tr[x];
+	return ret;
+}
 
 struct node
 {
-    int x, val;
-
-    bool operator<(const node& b) const
-    {
-        return x < b.x;
-    }
+	int l, r, id, cost;
+	inline bool operator <(node B) const { return r < B.r; }
 };
 
-inline void Zlin()
-{
-    int n, m, q;
-    cin >> n >> m >> q;
-    t.init(n);
-    vll V(n + 1);
-    set<ll> have;
-    vector<node> ss(m);
-    for (int i = 0; i < m; i++)
-        cin >> ss[i].x;
-    for (int i = 0; i < m; i++)
-        cin >> ss[i].val;
-    sort(ss.begin(), ss.end());
-    for (auto [x,val] : ss)
-    {
-        have.insert(x);
-        V[x] = val;
-    }
-    for (int i = 0; i < m - 1; i++)
-    {
-        ll len = ss[i + 1].x - ss[i].x - 1;
-        ll val = V[ss[i].x] * len * (len + 1) / 2;
-        t.upd(ss[i].x, val);
-    }
-    while (q--)
-    {
-        int op;
-        cin >> op;
-        if (op == 1)
-        {
-            ll x, v;
-            cin >> x >> v;
-            V[x] = v;
-            ll l = *prev(have.upper_bound(x));
-            ll r = *have.upper_bound(x);
-            // 更新l位置
-            ll len = r - l - 1;
-            ll val = V[l] * len * (len + 1) / 2;
-            t.upd(l, -val);
-            len = x - l - 1;
-            val = V[l] * len * (len + 1) / 2;
-            t.upd(l, val);
-            // 更新x位置
-            len = r - x - 1;
-            val = V[x] * len * (len + 1) / 2;
-            t.upd(x, val);
-            have.insert(x);
-        }
-        else
-        {
-            ll l, r;
-            cin >> l >> r;
-            ll lx = *have.lower_bound(l);
-            ll rx = *prev(have.upper_bound(r));
-            ll res = t.qry2(lx, rx - 1);
-            if (have.find(l) == have.end())
-            {
-                int len = lx - l;
-                int llx = *prev(have.find(lx));
-                res += V[llx] * len * (len + 1) / 2;
-            }
-            if (have.find(r) == have.end())
-            {
-                int len = r - rx;
-                int rrx = *next(have.find(rx));
-                int k = rrx - r;
-                res += V[rx] * len * (k + k + len - 1) / 2;
-            }
-            cout << res << endl;
-        }
-    }
-}
+inline int Sp(int x) { return x * (x + 1) / 2; }
 
-int main()
+signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    int ttt = 1;
-    while (ttt--) Zlin();
-    return 0;
+	scanf("%lld%lld%lld", &n, &m, &q);
+	multiset<node> S;
+	S.insert(node{1, n, 0, 0});
+	for (int i = 1; i <= m; i++) scanf("%lld", &X[i]);
+	for (int i = 1; i <= m; i++) scanf("%lld", &V[X[i]]);
+	for (int i = 1; i <= m; i++)
+	{
+		if (X[i] == 1 || X[i] == n) continue;
+		set<node>::iterator it = S.lower_bound(node{X[i], X[i], 0, 0});
+		S.insert(node{it->l, X[i], 0, 0});
+		S.insert(node{X[i], it->r, 0, 0});
+		S.erase(it);
+	}
+	for (int i = 1; i <= q; i++)
+	{
+		scanf("%lld", &opt[i]);
+		if (opt[i] == 1)
+		{
+			scanf("%lld%lld", &qx[i], &qv[i]);
+			V[qx[i]] = qv[i];
+			set<node>::iterator it = S.lower_bound(node{qx[i], qx[i], 0, 0});
+			S.insert(node{it->l, qx[i], 0, 0});
+			S.insert(node{qx[i], it->r, 0, 0});
+			S.erase(it);
+		}
+		else scanf("%lld%lld", &ql[i], &qr[i]);
+	}
+	int cnt = 0;
+	multiset<node> S2;
+	for (node N : S)
+	{
+		int l = N.l, r = N.r;
+		N.id = ++cnt, N.cost = V[l] * Sp(r - l - 1);
+		add(N.id, N.cost);
+		S2.insert(N);
+	}
+	S = S2;
+	for (int i = q; i >= 1; i--)
+	{
+		if (opt[i] == 2)
+		{
+			int L = ql[i], R = qr[i];
+			multiset<node>::iterator it2 = S.lower_bound(node{R, R, 0, 0});
+			multiset<node>::iterator it = S.lower_bound(node{L, L, 0, 0});
+			if (it->l == it2->l)
+			{
+				if (L == it->l) L++;
+				Ans[i] = V[it->l] * (Sp(R - L) + (R - L + 1) * (it->r - R));
+			}
+			else
+			{
+				int idl = it->id, idr = it2->id;
+				Ans[i] = sum(idr - 1) - sum(idl);
+				int l1 = it->l, l2 = it2->l, r1 = it->r, r2 = it2->r;
+				Ans[i] += V[l2] * (Sp(R - l2 - 1) + (R - l2) * (r2 - R));
+				if (L == l1) L++;
+				Ans[i] += V[l1] * Sp(r1 - L);
+			}
+		}
+		else
+		{
+			int x = qx[i];
+			multiset<node>::iterator it = S.lower_bound(node{x, x, 0, 0}), it2 = it;
+			it2++;
+			int idl = it->id, idr = it2->id;
+			add(idl, -(it->cost)), add(idr, -(it2->cost));
+			node New;
+			New.l = it->l, New.r = it2->r, New.id = idl, New.cost = V[New.l] * Sp(New.r - New.l - 1);
+			add(New.id, New.cost);
+			S.erase(it), S.erase(it2);
+			S.insert(New);
+		}
+	}
+	for (int i = 1; i <= q; i++) if (opt[i] == 2) printf("%lld\n", Ans[i]);
+	return 0;
 }
