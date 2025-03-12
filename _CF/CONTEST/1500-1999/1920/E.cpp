dce1870f2ab20e1b4cc2b5c26d1da96d9c2a5f46
27682 <2768228421@qq.com>
2025-03-12 11:56:00 +0800

    3.12

--- _CF/CONTEST/1500-1999/1920/E.cpp
+++ _CF/CONTEST/1500-1999/1920/E.cpp
@@ -12,16 +12,20 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long>pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
 
-inline void Zlin() {
+constexpr ll mo = 998244353;
 
+inline void Zlin()
+{
+    int n, k;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
