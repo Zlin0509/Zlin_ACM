6efa2c251c800d13e59b7237bca9a7bd26d58381
zlin <2768228421@qq.com>
2025-03-24 12:07:25 +0800

    3.24

--- _Luogu/P3808.cpp
+++ _Luogu/P3808.cpp
@@ -15,6 +15,76 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+
+struct AC_auto
+{
+private:
+    struct node
+    {
+        int val;
+        node* nex;
+        node* next[26];
+    };
+
+    node* root;
+
+public:
+    AC_auto() { root = new node(); }
+
+    void insert(const string& s, int val)
+    {
+        node* now = root;
+        for (char it : s)
+        {
+            if (now->next[it - 'a'] == nullptr)
+                now->next[it] = new node();
+            now = now->next[it];
+        }
+        now->val += val;
+    }
+
+    void build()
+    {
+        queue<node*> q;
+        for (int i = 0; i < 26; i++)
+            if (root->next[i] != nullptr)
+                q.push(root->next[i]);
+        while (!q.empty())
+        {
+            node* now = q.front();
+            q.pop();
+            for (int i = 0; i < 26; i++)
+            {
+                if (now->next[i] != nullptr)
+                {
+                    now->next[i]->nex = now->nex->next[i];
+                    q.push(now->next[i]);
+                }
+                else now->next[i] = now->nex->next[i];
+            }
+        }
+    }
+
+    int qry(const string& s)
+    {
+        int res = 0;
+        node* now = root;
+        node* cal;
+        for (char it : s)
+        {
+            now = now->next[it - 'a'];
+            cal = now;
+            while (cal != root && cal->val != -1)
+            {
+                res += cal->val;
+                cal->val = -1;
+                cal = cal->nex;
+            }
+        }
+        return res;
+    }
+} t;
+
 inline void Zlin()
 {
 }
