6d0e87fd5219f1791bc40e9388f306350ff01839
27682 <2768228421@qq.com>
2025-03-25 19:21:25 +0800

    3.25

--- _CF/CONTEST/1500-1999/1863/E.cpp
+++ _CF/CONTEST/1500-1999/1863/E.cpp
@@ -19,112 +19,71 @@ constexpr int N = 2e5 + 10;
 constexpr int inf = 1e9;
 
 int n, m, k;
-int h[N];
-int f[N], in1[N], in2[N], dis1[N], dis2[N];
-vi e1[N], e2[N], group[N];
+ll h[N], change[N], mx[N];
+int in[N];
+vi e[N], e1[N];
+vector<pll> have;
 
-inline int find(int u) { return f[u] == u ? u : f[u] = find(f[u]); }
-
-inline void merge(int x, int y) {
-    int fx = find(x), fy = find(y);
-    if (fx == fy) return;
-    if (fx > fy) swap(fx, fy);
-    f[fy] = fx;
+inline ll dfs(int u) {
+    if (change[u] != inf)
+        return change[u];
+    if (e1[u].empty())
+        return change[u] = h[u];
+    for (int v: e1[u])
+        if (h[u] - h[v] < k)
+            change[u] = min(change[u], dfs(v));
+    return change[u];
 }
 
-struct node {
-    int day, hour;
-};
-
-pll cal(const vi &have) {
+inline void topo() {
     queue<int> q;
-    for (int it: have)
-        if (!in1[it])
-            q.push(it);
+    for (int i = 1; i <= n; i++)
+        if (!in[i])
+            q.push(i);
     while (!q.empty()) {
         int u = q.front();
         q.pop();
-        for (int v: e1[u]) {
-            dis1[v] = max(dis1[v], dis1[u] + (h[u] > h[v] ? 1 : 0));
-            --in1[v];
-            if (!in1[v]) q.push(v);
+        for (int v: e[u]) {
+            if (h[v] < h[u])
+                h[v] += (h[u] - h[v] + k - 1) / k * k;
+            --in[v];
+            if (!in[v])
+                q.push(v);
         }
     }
-    for (int it: have)
-        if (!in2[it])
-            q.push(it);
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for (int v: e2[u]) {
-            dis2[v] = max(dis2[v], dis2[u] + (h[v] > h[u] ? 1 : 0));
-            --in2[v];
-            if (!in2[v]) q.push(v);
-        }
-    }
-
-    int tag = 0;
-    for (int it: have)
-        tag = max({tag, dis1[it], dis2[it]});
-    node lx{0, k}, rx{tag, 0};
-    for (int it: have) {
-        if (dis1[it] == tag) {
-            rx.hour = max(rx.hour, h[it]);
-        }
-        if (dis2[it] == tag) {
-            lx.hour = min(lx.hour, h[it]);
-        }
-    }
-    return {lx.hour, 1ll * tag * k + rx.hour};
+    for (int i = 1; i <= n; i++)
+        if (e[i].empty())
+            have.push_back({dfs(i), h[i]});
 }
 
 inline void Zlin() {
     cin >> n >> m >> k;
+    have.clear();
     for (int i = 1; i <= n; i++) {
         cin >> h[i];
-        group[i].clear();
-        e1[i].clear(), e2[i].clear();
-        in1[i] = in2[i] = dis1[i] = dis2[i] = 0;
-        f[i] = i;
+        e[i].clear();
+        e1[i].clear();
+        in[i] = mx[i] = 0;
+        change[i] = inf;
     }
-    while (m--) {
-        int a, b;
+    for (int i = 1, a, b; i <= m; i++) {
         cin >> a >> b;
-        merge(a, b);
-        e1[a].push_back(b);
-        e2[b].push_back(a);
-        in1[b]++, in2[a]++;
+        e[a].push_back(b);
+        ++in[b];
+        e1[b].push_back(a);
     }
-    int cnt = 0;
-    while (1) {
-        vi vis(n + 1);
-        for (int i = 1; i <= n; i++) {
-            if (!vis[find(i)]) vis[f[i]] = ++cnt;
-            group[vis[f[i]]].push_back(i);
+    topo();
+    sort(have.begin(), have.end());
+    for (int i = have.size() - 1; i >= 0; i--)
+        mx[i] = max(mx[i + 1], have[i].second);
+    ll ans = 1e16, l = 0, r = 0;
+    for (int i = 0; i < have.size();) {
+        ans = min(ans, max(r, mx[i]) - have[i].first);
+        l = have[i].first;
+        while (i < have.size() && have[i].first == l) {
+            r = max(r, have[i].second + k);
+            ++i;
         }
-        break;
-    }
-
-    multiset<ll> l, r;
-    vector<pll> q;
-    for (int i = 1; i <= cnt; i++) {
-        pll u = cal(group[i]);
-        l.insert(u.first);
-        r.insert(u.second);
-        q.push_back(u);
-    }
-    sort(q.begin(), q.end());
-    ll ans = *prev(r.end()) - *l.begin();
-    for (auto [lx, rx]: q) {
-        l.erase(l.find(lx));
-        r.erase(r.find(rx));
-        lx += k;
-        rx += k;
-        l.insert(lx);
-        r.insert(rx);
-        lx = min(lx, *l.begin());
-        rx = max(rx, *prev(r.end()));
-        ans = min(ans, rx - lx);
     }
     cout << ans << endl;
 }
