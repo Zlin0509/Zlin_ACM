1456ddaa179eae4ff4f00f9e0c09229b367bbfbe
27682 <2768228421@qq.com>
2025-03-25 09:33:44 +0800

    3.25

---
+++ _CF/CONTEST/1500-1999/1864/E.cpp
@@ -0,0 +1,55 @@
+//
+// Created by 27682 on 2025/3/25.
+//
+#include "bits/stdc++.h"
+#define endl '\n'
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+constexpr ll mo = 998244353;
+
+inline ll qpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % mo;
+        a = a * a % mo;
+        b >>= 1;
+    }
+    return res;
+}
+
+inline void Zlin() {
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int &it: a) cin >> it;
+    vector f0(32, vi(32, 0)), f1(32, vi(32, 0));
+    for (int it: a) {
+        for (int i = 30, cnt = 0; ~i; i--) {
+            if (it >> i & 1) f1[i][cnt++]++;
+            else f0[i][cnt]++;
+        }
+    }
+    ll ans = 0, val;
+    for (int i = 0; i < 30; i++) {
+        for (int j = 0; j < 30; j++) {
+            val = f0[i][j] * f1[i][j];
+            val = val * (6 * j + 3);
+            ans = (ans + val) % mo;
+        }
+    }
+    ans = ans * qpow(1ll * n * n, mo - 2) % mo;
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int ttt = 1;
+    cin >> ttt;
+    while (ttt--) Zlin();
+    return 0;
+}
