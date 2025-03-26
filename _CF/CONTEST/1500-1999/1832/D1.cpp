d71aba5d9a196b7cdb858e3a14348b607a677e1c
Zlin_PC <zlin0509@outlook.com>
2025-03-26 11:10:12 +0800

    3.25

--- _CF/CONTEST/1500-1999/1832/D1.cpp
+++ _CF/CONTEST/1500-1999/1832/D1.cpp
@@ -13,6 +13,22 @@ inline void Zlin() {
     sort(a.begin(), a.end());
     for (int i = 1, k; i <= q; i++) {
         cin >> k;
+        vector<int> b = a;
+        if (k <= n) {
+            for (int j = 0; j < k; j++)
+                b[j] += k - j;
+            sort(b.begin(), b.end());
+            cout << b[0] << ' ';
+            continue;
+        }
+        for (int j = 0; j < n - 1; j++) b[j] += k - j;
+        if ((n + k) % 2 == 0) b[n - 1] += k - n + 1;
+        sort(b.begin(), b.end());
+        long long sum = 0;
+        for (int j = 1; j < n; j++) sum += b[j] - b[0];
+        double f = max(0.0, (k - (n - (n + k) % 2)) / 2.0 - sum);
+        int ans = ceil(f / n);
+        cout << b[0] - ans << ' ';
     }
 }
 
