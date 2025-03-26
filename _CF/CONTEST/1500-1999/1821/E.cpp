c8d885170cbf658d33305fdb460b660745a2c47b
Zlin_PC <zlin0509@outlook.com>
2025-03-26 11:46:08 +0800

    3.26

--- _CF/CONTEST/1500-1999/1821/E.cpp
+++ _CF/CONTEST/1500-1999/1821/E.cpp
@@ -16,6 +16,34 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 inline void Zlin() {
+    int k;
+    cin >> k;
+    string s;
+    cin >> s;
+    s = ' ' + s;
+    vi cnt(s.length()), all;
+    stack<int> have;
+    ll ans = 0;
+    for (int r = 1, val, l; r < s.length(); r++) {
+        cnt[r] += cnt[r - 1];
+        if (s[r] == '(') {
+            have.push(r);
+        } else {
+            l = have.top();
+            have.pop();
+            val = cnt[r] - cnt[l];
+            ans += val;
+            all.push_back(val);
+            ++cnt[r];
+        }
+    }
+    sort(all.begin(), all.end());
+    while (k && !all.empty()) {
+        ans -= all.back();
+        all.pop_back();
+        --k;
+    }
+    cout << ans << endl;
 }
 
 int main() {
