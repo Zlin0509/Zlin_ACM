0775f5e4c3ef8f379e2de8439462b0a563933d7e
Zlin_PC <zlin0509@outlook.com>
2025-03-26 15:15:40 +0800

    3.26

--- _CF/CONTEST/1500-1999/1811/F.cpp
+++ _CF/CONTEST/1500-1999/1811/F.cpp
@@ -22,42 +22,31 @@ vi e[N];
 
 int dfn[N], low[N], tot;
 int stk[N], instk[N], top;
-int scc[N], cnt;
 bool cut[N];
 
 void init() {
-    tot = top = cnt = 0;
+    tot = top = 0;
     for (int i = 1; i <= n; i++) {
         e[i].clear();
         dfn[i] = low[i] = 0;
         stk[i] = instk[i] = 0;
-        scc[i] = 0;
         cut[i] = false;
     }
 }
 
-void tarjan(int x) {
+void tarjan(int x, int s) {
     int ss = 0;
     dfn[x] = low[x] = ++tot;
     stk[++top] = x, instk[x] = 1;
     for (int y: e[x]) {
         if (!dfn[y]) {
             ++ss;
-            tarjan(y);
+            tarjan(y, s);
             low[x] = min(low[x], low[y]);
             if (low[y] >= dfn[x]) cut[x] = true;
         } else if (instk[y]) low[x] = min(low[x], dfn[y]);
     }
-    if (dfn[x] == low[x]) {
-        int y;
-        ++cnt;
-        do {
-            y = stk[top--];
-            instk[y] = 0;
-            scc[y] = cnt;
-        } while (y != x);
-    }
-    if (ss == 1 && x == 1) cut[x] = false;
+    if (ss == 1 && x == s) cut[x] = false;
 }
 
 inline void Zlin() {
