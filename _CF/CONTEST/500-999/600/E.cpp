1c5afe1555fec5c92d5075f24f8d65c339063aed
Zlin_PC <zlin0509@outlook.com>
2025-03-27 18:57:46 +0800

    3.27

---
+++ _CF/CONTEST/500-999/600/E.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 27682 on 2025/3/27.
+//
+
+#include "bits/stdc++.h"
+#define endl '\n'
+using namespace std;
+
+typedef __int128 i128;
+typedef long long i64;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef vector<int> vi;
+typedef vector<i64> vll;
+typedef pair<int, int> pii;
+typedef pair<i64, i64> pll;
+
+inline void Zlin() {
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int ttt = 1;
+    cin >> ttt;
+    while (ttt--) Zlin();
+    return 0;
+}
