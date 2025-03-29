4d1559649d338ffae8a9c1154b20f1ec3b867e2c
Zlin_PC <zlin0509@outlook.com>
2025-03-29 13:17:30 +0800

    3.29

--- _CF/CONTEST/2000-2499/2056/D.cpp
+++ _CF/CONTEST/2000-2499/2056/D.cpp
@@ -19,8 +19,31 @@ typedef pair<ll, ll> pll;
 inline void Zlin() {
     int n;
     cin >> n;
-    vi a(n);
-    for (int &it: a) cin >> it;
+    vi a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    ll ans = 1ll * n * (n + 1) / 2;
+    for (int i = 1; i <= 10; i++) {
+        vi b(n + 1), nxt(n + 1);
+        int lx = 0;
+        for (int j = 1; j <= n; j++) {
+            b[j] = a[j] <= i ? 1 : 0;
+            b[j] += b[j - 1];
+            if (a[j] == i) lx = j;
+            nxt[j] = lx - 1;
+        }
+        int tag = 0;
+        unordered_map<int, int> buf[2];
+        for (int j = 1; j <= n; j++) {
+            while (tag <= nxt[j]) {
+                buf[0][2 * b[tag] - tag]++;
+                buf[1][2 * b[tag] - tag]++;
+                ++tag;
+            }
+            ans -= buf[j & 1][2 * b[j] - j];
+        }
+    }
+    cout << ans << endl;
 }
 
 int main() {
