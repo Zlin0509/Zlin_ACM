ed02e88f4423e375e667f37a0c41c65ea33d8ae9
zlin <2768228421@qq.com>
2025-04-08 09:06:47 +0800

    4,8

--- _CF/CONTEST/1500-1999/1946/E.cpp
+++ _CF/CONTEST/1500-1999/1946/E.cpp
@@ -16,13 +16,59 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+constexpr ll mo = 1e9 + 7;
+constexpr int N = 2e5;
+
+ll fac[N + 10], inv[N + 10];
+
+inline ll qpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % mo;
+        a = a * a % mo;
+        b >>= 1;
+    }
+    return res;
+}
+
+inline void dd() {
+    fac[0] = inv[0] = 1;
+    for (int i = 1; i <= N; i++)
+        fac[i] = fac[i - 1] * i % mo;
+    inv[N] = qpow(fac[N], mo - 2);
+    for (int i = N - 1; i; i--)
+        inv[i] = inv[i + 1] * (i + 1) % mo;
+}
+
+inline ll comb(int n, int k) {
+    return fac[n] * inv[k] % mo * inv[n - k] % mo;
+}
+
+
 inline void Zlin() {
+    int n, ml, mr;
+    cin >> n >> ml >> mr;
+    vi lx(ml), rx(mr);
+    for (int &it: lx) cin >> it;
+    for (int &it: rx) cin >> it;
+    if (lx[ml - 1] != rx[0]) {
+        cout << 0 << endl;
+        return;
+    }
+    ll al = 1, ar = 1;
+    for (int i = 1; i < ml; i++)
+        al = al * fac[lx[i] - lx[i - 1] - 1] % mo;
+    for (int i = 1; i < mr; i++)
+        ar = ar * fac[rx[i] - rx[i - 1] - 1] % mo;
+    ll ans = al * ar % mo * comb(n - 1, lx.back() - 1) % mo;
+    cout << ans << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
+    dd();
     cin >> ttt;
     while (ttt--) Zlin();
     return 0;
