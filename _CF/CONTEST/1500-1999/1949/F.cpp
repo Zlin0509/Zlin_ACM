3223aaeb61118a4980dfdc86d10d603f8f2ef42a
zlin <2768228421@qq.com>
2025-04-08 08:33:27 +0800

    4,8

--- _CF/CONTEST/1500-1999/1949/F.cpp
+++ _CF/CONTEST/1500-1999/1949/F.cpp
@@ -19,14 +19,43 @@ typedef pair<ll, ll> pll;
 inline void Zlin() {
     int n, m;
     cin >> n >> m;
-    vector<vi> a;
+    vector<pair<vi, int> > a;
+    for (int i = 0, k; i < n; i++) {
+        cin >> k;
+        vi now(k);
+        for (int &it: now) cin >> it, --it;
+        a.emplace_back(now, i + 1);
+    }
+    sort(a.begin(), a.end(), [&](const pair<vi, int> &a, const pair<vi, int> &b) {
+        return a.first.size() < b.first.size();
+    });
+    vi lst(m, -1), vis(n, 0);
+    for (int i = 0; i < n; i++) {
+        vi s;
+        for (int it: a[i].first) {
+            if (lst[it] == -1) {
+                lst[it] = i;
+                continue;
+            }
+            if (!vis[lst[it]]) s.emplace_back(lst[it]);
+            vis[lst[it]]++;
+            lst[it] = i;
+        }
+        for (int it: s) {
+            if (vis[it] != a[it].first.size()) {
+                cout << "YES" << endl;
+                cout << a[i].second << ' ' << a[it].second << endl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << endl;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     int ttt = 1;
-    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
