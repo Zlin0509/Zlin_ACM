82aa55dd139c9a5729e5898d2b607f24c3615a79
Zlin_PC <zlin0509@outlook.com>
2025-04-09 11:57:57 +0800

    4.9

--- _CF/CONTEST/1500-1999/1909/F1.cpp
+++ _CF/CONTEST/1500-1999/1909/F1.cpp
@@ -16,12 +16,36 @@ typedef vector<ll> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-inline void Zlin()
-{
+constexpr ll mo = 998244353;
+
+inline void Zlin() {
+    int n;
+    cin >> n;
+    vi a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    if (a[n] != n) {
+        cout << 0 << endl;
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i + 1] - a[i] > 2 || a[i + 1] - a[i] < 0) {
+            cout << 0 << endl;
+            return;
+        }
+    }
+
+    ll ans = 1;
+    for (int i = 1; i <= n; i++) {
+        if (a[i] == a[i - 1] + 1) {
+            ans = ans * ((i - a[i - 1]) * 2 - 1) % mo;
+        } else if (a[i] == a[i - 1] + 2) {
+            ans = ans * (i - a[i - 1] - 1) % mo * (i - a[i - 1] - 1) % mo;
+        }
+    }
+    cout << ans << endl;
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     int ttt = 1;
