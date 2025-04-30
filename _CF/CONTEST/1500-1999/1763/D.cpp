b9161c144c216fb488b523bc6d0371d8d8198a18
zlin <2768228421@qq.com>
2025-05-01 00:37:29 +0800

    daily

--- _CF/CONTEST/1500-1999/1763/D.cpp
+++ _CF/CONTEST/1500-1999/1763/D.cpp
@@ -16,7 +16,46 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+constexpr ll mo = 1e9 + 7;
+
+int n, I, J, X, Y, ans;
+
 inline void Zlin() {
+    cin >> n >> I >> J >> X >> Y;
+    if (X > Y) {
+        X ^= Y ^= X ^= Y;
+        I ^= J ^= I ^= J;
+    }
+    vector dp(n + 1, vi(n + 1, 0));
+    dp[0][0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (i == X) {
+            if (X >= I)
+                dp[i][I] += dp[i - 1][I - 1];
+            if (X - (n - I + 1) >= 0)
+                dp[i][X - (n - I + 1)] += dp[i - 1][X - (n - I + 1)];
+        } else if (i == Y) {
+            if (Y >= J)
+                dp[i][J] += dp[i - 1][J - 1];
+            if (Y - (n - J + 1) >= 0)
+                dp[i][Y - (n - J + 1)] += dp[i - 1][Y - (n - J + 1)];
+        } else {
+            for (int j = 0; j < i; j++) {
+                dp[i][j] += dp[i - 1][j];
+                dp[i][j] %= mo;
+                dp[i][j + 1] += dp[i - 1][j];
+                dp[i][j + 1] %= mo;
+            }
+        }
+    }
+    if (Y != n) {
+        for (int i = 1; i <= n - 2; i++) {
+            ans += dp[n - 1][i];
+            ans %= mo;
+        }
+    } else if (J != n && J != 1) ans = dp[n - 1][J - 1];
+    else ans = 0;
+    cout << ans << endl;
 }
 
 int main() {
