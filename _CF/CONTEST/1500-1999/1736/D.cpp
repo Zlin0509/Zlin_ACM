25d4d583868782da39af9c520958dfff10f0681d
zlin <2768228421@qq.com>
2025-05-01 03:07:05 +0800

    daily

--- _CF/CONTEST/1500-1999/1736/D.cpp
+++ _CF/CONTEST/1500-1999/1736/D.cpp
@@ -13,16 +13,15 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long>pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
 
 inline void Zlin() {
-
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
