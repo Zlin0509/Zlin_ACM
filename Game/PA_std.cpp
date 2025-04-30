04a3294f89e6b0c7c35d3639cae3a9fc4256b78b
zlin <2768228421@qq.com>
2025-05-01 03:06:53 +0800

    daily

---
+++ Game/PA_std.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Zlin on 2025/5/1.
+//
+
+#include "bits/stdc++.h"
+#define endl "\n"
+using namespace std;
+
+typedef __int128 i128;
+typedef double db;
+typedef long double ldb;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+constexpr ll inf = 2e18;
+
+int n;
+ll a1, b1, a2, b2;
+
+ll cost(ll a, ll b, ll k) {
+    return a * k * (k + 1) / 2 + b * k;
+}
+
+ll total_cost(ll x) {
+    ll y = n - x;
+    return cost(a1, b1, x) + cost(a2, b2, y);
+}
+
+inline void Zlin() {
+    cin >> n >> a1 >> b1 >> a2 >> b2;
+    int l = 1, r = n - 1;
+    while (r - l > 3) {
+        int midl = l + (r - l) / 3;
+        int midr = r - (r - l) / 3;
+        if (total_cost(midl) < total_cost(midr))
+            r = midr - 1;
+        else
+            l = midl + 1;
+    }
+    ll ans = inf;
+    for (int i = l; i <= r; i++) {
+        ans = min(ans, total_cost(i));
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int ttt = 1;
+    cin >> ttt;
+    while (ttt--) Zlin();
+    return 0;
+}
