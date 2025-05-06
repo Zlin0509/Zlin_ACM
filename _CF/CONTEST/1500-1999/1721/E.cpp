61ceadb51c81377e780d4d62b484f70a1bbb7232
Zlin_PC <zlin0509@outlok.com>
2025-05-06 18:25:02 +0800

    daily

--- _CF/CONTEST/1500-1999/1721/E.cpp
+++ _CF/CONTEST/1500-1999/1721/E.cpp
@@ -16,27 +16,27 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 constexpr int N = 1e6 + 50;
-constexpr ull p = 13114511;
+constexpr ll p = 1145141, mo = 1e9 + 7;
 
-ull Hash[N], fac[N];
-ull lx, rx, va;
+ll Hash[N], fac[N];
+ll lx, rx, va;
 string s, t;
-int q, ans[N];
+int q, ans[12];
 
-unordered_map<ull, int> val;
+unordered_map<ll, int> val;
 
 inline void Zlin() {
     fac[0] = 1;
-    for (int i = 1; i < N; i++) fac[i] = fac[i - 1] * p;
+    for (int i = 1; i < N; i++) fac[i] = fac[i - 1] * p % mo;
     cin >> s;
     Hash[0] = s[0];
-    for (int i = 1; i < s.length(); i++) Hash[i] = Hash[i - 1] * p + s[i];
+    for (int i = 1; i < s.length(); i++) Hash[i] = (Hash[i - 1] * p + s[i]) % mo;
     for (int len = 1; len <= 10; len++) {
         for (int k = len + 1; k <= s.length(); k++) {
             lx = Hash[k - 1 - len];
-            rx = Hash[s.length() - 1] - Hash[s.length() - 1 - (k - len)] * fac[k - len];
+            rx = (Hash[s.length() - 1] - Hash[s.length() - 1 - (k - len)] * fac[k - len] % mo + mo) % mo;
             if (lx == rx) {
-                va = Hash[k - 1] - Hash[k - 1 - len] * fac[len];
+                va = (Hash[k - 1] - Hash[k - 1 - len] * fac[len] % mo + mo) % mo;
                 val[va] = max(val[va], k);
             }
         }
@@ -44,13 +44,18 @@ inline void Zlin() {
     cin >> q;
     while (q--) {
         cin >> t;
-        for (int i = 0; i < t.length(); i++) Hash[i + s.length()] = Hash[i + s.length() - 1] * p + t[i];
+        for (int i = 0; i < t.length(); i++) Hash[i + s.length()] = (Hash[i + s.length() - 1] * p + t[i]) % mo;
         for (int i = 0; i < t.length(); i++) {
-            va = Hash[s.length() + i] - Hash[s.length() - 1] * fac[i + 1];
+            va = (Hash[s.length() + i] - Hash[s.length() - 1] * fac[i + 1] % mo + mo) % mo;
             val.contains(va) ? ans[i + 1] = val[va] : ans[i + 1] = 0;
             for (int j = 0; j <= min(20, (int) s.length() + i - 1); j++) {
                 lx = Hash[j];
-                rx = Hash[s.length() + i] - Hash[s.length() + i - (j + 1)] * fac[j + 1];
+                rx = (Hash[s.length() + i] - Hash[s.length() + i - (j + 1)] * fac[j + 1] % mo + mo) % mo;
+                if (lx == rx) ans[i + 1] = max(ans[i + 1], j + 1);
+            }
+            for (int j = s.length(); j <= s.length() + i - 1; j++) {
+                lx = Hash[j];
+                rx = (Hash[s.length() + i] - Hash[s.length() + i - (j + 1)] * fac[j + 1] % mo + mo) % mo;
                 if (lx == rx) ans[i + 1] = max(ans[i + 1], j + 1);
             }
         }
