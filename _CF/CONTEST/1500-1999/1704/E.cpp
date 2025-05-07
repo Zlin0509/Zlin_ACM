bfbf7617bdb7ef1e50618d20c392ad8130c888b8
zlin <2768228421@qq.com>
2025-05-07 14:59:49 +0800

    daily

--- _CF/CONTEST/1500-1999/1704/E.cpp
+++ _CF/CONTEST/1500-1999/1704/E.cpp
@@ -13,16 +13,51 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long>pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+constexpr int N = 1010;
+constexpr ll mo = 998244353;
+
+int n, m, in[N], vis[N];
+ll a[N], dep[N];
+vi e[N];
+
+inline void dfs(int u) {
+    if (vis[u]) return;
+    vis[u] = 1;
+    for (int v: e[u]) dfs(v);
+    bool tag = a[u];
+    for (int v: e[u]) {
+        a[u] += a[v];
+        dep[u] = max(dep[u], dep[v] + a[v]);
+    }
+    a[u] -= dep[u] - !tag;
+}
 
 inline void Zlin() {
-
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        in[i] = dep[i] = vis[i] = 0;
+        e[i].clear();
+    }
+    for (int i = 1, u, v; i <= m; i++) {
+        cin >> v >> u;
+        e[u].push_back(v);
+        ++in[v];
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!in[i]) {
+            dfs(i);
+            cout << (a[i] + dep[i] + 1) % mo << endl;
+        }
+    }
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
