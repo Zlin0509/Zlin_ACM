1fafa38f844f56032f2863a6c26baac45b3f69a9
Zlin_PC <zlin0509@outlok.com>
2025-05-08 11:05:35 +0800

    daily

--- _CF/CONTEST/2000-2499/2089/B2.cpp
+++ _CF/CONTEST/2000-2499/2089/B2.cpp
@@ -15,31 +15,70 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-constexpr int N = 4e5 + 10;
+constexpr int N = 2e5 + 10;
 
-int n, a[N], b[N], val[N];
-ll k, sa[N], sb[N];
+int n, mx, a[N], b[N];
+ll k, sum;
+
+struct {
+    ull val[3200];
+
+
+    void init() {
+        for (int i = 0; i <= mx; i++) val[i] = -1;
+    }
+
+    void change(int i) {
+        val[i >> 6] ^= 1ull << (i & 63);
+    }
+
+    ull query(int i) {
+        return val[i >> 6] >> (i & 63) & 1;
+    }
+
+    void move() {
+        ull q = query(n - 1);
+        for (int i = mx; i; i--) val[i] <<= 1, val[i] |= val[i - 1] >> 63;
+        val[0] <<= 1, val[0] |= q;
+    }
+} ax, bx;
 
 inline void Zlin() {
     cin >> n >> k;
-    for (int i = 1; i <= n; i++) {
+    sum = 0;
+    mx = n - 1 >> 6;
+    ax.init(), bx.init();
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
-        a[i + n] = a[i];
+        sum += a[i];
     }
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> b[i];
-        b[i + n] = b[i];
     }
-    for (int i = 1; i <= n << 1; i++) {
-        sa[i] = sa[i - 1] + a[i];
-        sb[i] = sb[i - 1] + b[i];
+    if (sum <= k) {
+        cout << 0 << endl;
+        return;
     }
     int ans = 0;
-    for (int i = 1, rx; i <= n; i++) {
-        rx = max(i, val[i - 1]);
-        while (sa[rx] - sa[i - 1] > sb[rx] - sb[i - 1]) ++rx;
-        val[i] = rx;
-        ans = max(ans, rx - i + 1);
+    while (sum > k) {
+        for (int i = 0; i <= mx; i++) {
+            ull z = ax.val[i] & bx.val[i];
+            if (z) {
+                int pre = i << 6;
+                for (int j = 0, lx, rx, t; j < 63 && pre + j < n; j++) {
+                    if (z >> j & 1) {
+                        rx = pre + j;
+                        lx = (rx - ans + n) % n;
+                        t = min(a[lx], b[rx]);
+                        a[lx] -= t, b[rx] -= t, sum -= t;
+                        if (!a[lx]) ax.change(rx);
+                        if (!b[rx]) bx.change(rx);
+                    }
+                }
+            }
+        }
+        ++ans;
+        ax.move();
     }
     cout << ans << endl;
 }
