f656b08e4b2ed3d76c0a16909b6f5ad091538438
Zlin_PC <zlin0509@outlok.com>
2025-05-10 20:01:46 +0800

    daily

--- _CF/CONTEST/2000-2499/2043/E.cpp
+++ _CF/CONTEST/2000-2499/2043/E.cpp
@@ -15,10 +15,43 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-int A[1001][1001], B[1001][1001];
-int a[1001][1001], b[1001][1001];
-int sum[1001], used[1001];
+int A[1005][1005], B[1005][1005];
+int a[1005][1005], b[1005][1005];
 int n, m;
+
+inline void opa() {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cout << a[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+inline void opb() {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cout << b[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+vi e[2010];
+int used[2010], vis[2010];
+
+bool dfs(int u) {
+    vis[u] = 1;
+    for (int v: e[u]) {
+        if (vis[v] == 0) {
+            if (dfs(v)) return true;
+        } else if (vis[v] == 1) return true;
+    }
+    vis[u] = 2;
+    return false;
+}
 
 inline void Zlin() {
     cin >> n >> m;
@@ -32,111 +65,40 @@ inline void Zlin() {
             cin >> B[i][j];
         }
     }
-    bool check = true;
     for (int k = 0; k < 32; k++) {
-        for (int j = 1; j <= m; j++) sum[j] = 0;
+        for (int i = 1; i <= n + m; i++) {
+            used[i] = vis[i] = 0;
+            e[i].clear();
+        }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 a[i][j] = A[i][j] >> k & 1;
                 b[i][j] = B[i][j] >> k & 1;
             }
         }
+        // cout << k << endl;
+        // opa(), opb();
         for (int i = 1; i <= n; i++) {
-            // 全零直接跳过
-            bool tag = true;
             for (int j = 1; j <= m; j++) {
                 if (b[i][j]) {
-                    tag = false;
-                    break;
+                    e[i].push_back(i + j);
                 }
-            }
-            if (tag) {
-                for (int j = 1; j <= m; j++) ++sum[j];
-                continue;
-            }
-            // 判断是否需要先&0
-            tag = true;
-            for (int j = 1; j <= m; j++) {
-                if (a[i][j] | used[j] && !b[i][j]) {
-                    tag = false;
-                    break;
+                if (!b[i][j]) {
+                    e[i + j].push_back(i);
                 }
-            }
-            // 想要进行|1操作必须前面位置全为1
-            for (int j = 1; j <= m; j++) {
-                if (a[i][j] | used[j] & tag == b[i][j]) {
-                    sum[j] += b[i][j];
-                    continue;
+                if (a[i][j] != b[i][j]) {
+                    b[i][j] ? used[i + j] = 1 : used[i] = 1;
                 }
-                if (sum[j] != i - 1) {
-                    check = false;
-                    break;
-                }
-                sum[j]++;
-                a[i][j] = 1;
-                used[j] = 1;
-            }
-            if (!check) break;
-        }
-        if (!check) break;
-    }
-    if (check) {
-        cout << "Yes" << endl;
-        return;
-    }
-    check = true;
-    for (int k = 0; k < 32; k++) {
-        for (int j = 1; j <= m; j++) sum[j] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                a[i][j] = A[i][m - j + 1] >> k & 1;
-                b[i][j] = B[i][m - j + 1] >> k & 1;
             }
         }
-        for (int i = 1; i <= n; i++) {
-            // 全零直接跳过
-            bool tag = true;
-            for (int j = 1; j <= m; j++) {
-                if (b[i][j]) {
-                    tag = false;
-                    break;
-                }
+        for (int i = 1; i <= n + m; i++) {
+            if (used[i] && !vis[i] && dfs(i)) {
+                cout << "No" << endl;
+                return;
             }
-            if (tag) {
-                for (int j = 1; j <= m; j++) ++sum[j];
-                continue;
-            }
-            // 判断是否需要先&0
-            tag = true;
-            for (int j = 1; j <= m; j++) {
-                if (a[i][j] | used[j] && !b[i][j]) {
-                    tag = false;
-                    break;
-                }
-            }
-            // 想要进行|1操作必须前面位置全为1
-            for (int j = 1; j <= m; j++) {
-                if (a[i][j] | used[j] & tag == b[i][j]) {
-                    sum[j] += b[i][j];
-                    continue;
-                }
-                if (sum[j] != i - 1) {
-                    check = false;
-                    break;
-                }
-                sum[j]++;
-                a[i][j] = 1;
-                used[j] = 1;
-            }
-            if (!check) break;
         }
-        if (!check) break;
     }
-    if (check) {
-        cout << "Yes" << endl;
-        return;
-    }
-    cout << "No" << endl;
+    cout << "Yes" << endl;
 }
 
 int main() {
