574ac3d2ecf9d652fc8fd425e657b4d039cd1688
Zlin_PC <zlin0509@outlok.com>
2025-05-10 16:24:14 +0800

    daily

--- _CF/CONTEST/2000-2499/2057/E1.cpp
+++ _CF/CONTEST/2000-2499/2057/E1.cpp
@@ -12,16 +12,29 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long> pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int f[404][404][404];
+
+struct line {
+    int u, v, val;
+};
 
 inline void Zlin() {
-
+    int n, m, q;
+    cin >> n >> m >> q;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            for (int k = 1; k <= m; k++) {
+            }
+        }
+    }
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
