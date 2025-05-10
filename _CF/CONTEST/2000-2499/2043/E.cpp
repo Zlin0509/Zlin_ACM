d57e66ac1bb2a53bb80bb7cbc7ea99b7ac3dd520
Zlin_PC <zlin0509@outlok.com>
2025-05-10 17:24:30 +0800

    daily

--- _CF/CONTEST/2000-2499/2043/E.cpp
+++ _CF/CONTEST/2000-2499/2043/E.cpp
@@ -12,16 +12,77 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long> pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int A[1001][1001], B[1001][1001];
+int a[1001][1001], b[1001][1001];
+int sum[1001], used[1001];
+int n, m;
 
 inline void Zlin() {
-
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> A[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> B[i][j];
+        }
+    }
+    for (int k = 0; k < 32; k++) {
+        for (int j = 1; j <= m; j++) sum[j] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                a[i][j] = A[i][j] >> k & 1;
+                b[i][j] = B[i][j] >> k & 1;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            // 全零直接跳过
+            bool tag = true;
+            for (int j = 1; j <= m; j++) {
+                if (b[i][j]) {
+                    tag = false;
+                    break;
+                }
+            }
+            if (tag) {
+                for (int j = 1; j <= m; j++) ++sum[j];
+                continue;
+            }
+            // 判断是否需要先&0
+            tag = true;
+            for (int j = 1; j <= m; j++) {
+                if (a[i][j] | used[j] && !b[i][j]) {
+                    tag = false;
+                    break;
+                }
+            }
+            // 想要进行|1操作必须前面位置全为1
+            for (int j = 1; j <= m; j++) {
+                if (a[i][j] | used[j] & tag == b[i][j]) {
+                    sum[j] += b[i][j];
+                    continue;
+                }
+                if (sum[j] != i - 1) {
+                    cout << "No" << endl;
+                    return;
+                }
+                sum[j]++;
+                a[i][j] = 1;
+                used[j] = 1;
+            }
+        }
+    }
+    cout << "Yes" << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
