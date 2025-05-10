ab2d21f340a9a6db6dea2ce6c71e1ca8cb46d63c
Zlin_PC <zlin0509@outlok.com>
2025-05-10 16:08:04 +0800

    daily

--- _CF/CONTEST/2000-2499/2063/E.cpp
+++ _CF/CONTEST/2000-2499/2063/E.cpp
@@ -12,16 +12,62 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long> pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+constexpr int N = 3e5 + 10;
+
+int n, dep[N], siz[N];
+vi e[N];
+
+ll ans;
+
+inline void dfs(int u,int fa) {
+    ans += 1ll * (n - 1) * dep[u];
+    ll cnt = 0;
+    siz[u] = 1;
+    for (int v: e[u]) {
+        if (v == fa) continue;
+        dep[v] = dep[u] + 1;
+        dfs(v, u);
+        cnt += 1ll * siz[u] * siz[v];
+        siz[u] += siz[v];
+    }
+    ans -= 2ll * dep[u] * cnt;
+    ans += siz[u] - 1;
+}
 
 inline void Zlin() {
-
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        dep[i] = siz[i] = 0;
+        e[i].clear();
+    }
+    for (int i = 1, u, v; i < n; i++) {
+        cin >> u >> v;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+    ans = -1ll * n * (n - 1) / 2;
+    dfs(1, 0);
+    ll sr = 0, cntr = 0;
+    vi val;
+    for (int i = 1; i <= n; i++) {
+        val.push_back(dep[i]);
+        sr += dep[i], ++cntr;
+    }
+    sort(val.begin(), val.end());
+    for (int it: val) {
+        sr -= it;
+        --cntr;
+        ans -= sr - cntr * it;
+    }
+    cout << ans << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
