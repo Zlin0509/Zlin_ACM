46f95940492068c59e72b722acaf896ed166b97f
Zlin_PC <zlin0509@outlok.com>
2025-05-10 16:51:25 +0800

    daily

--- _CF/CONTEST/2000-2499/2057/E1.cpp
+++ _CF/CONTEST/2000-2499/2057/E1.cpp
@@ -15,11 +15,7 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-int f[404][404][404];
-
-struct line {
-    int u, v, val;
-};
+int f[404][404][404], g[404][404][404];
 
 inline void Zlin() {
     int n, m, q;
@@ -31,7 +27,54 @@ inline void Zlin() {
             }
         }
     }
-
+    map<int, vector<pii> > mp;
+    vi have;
+    for (int i = 1, u, v, val; i <= m; i++) {
+        cin >> u >> v >> val;
+        mp[val].push_back({u, v});
+        have.push_back(val);
+    }
+    sort(have.begin(), have.end());
+    have.erase(unique(have.begin(), have.end()), have.end());
+    reverse(have.begin(), have.end());
+    for (int it: have) {
+        for (auto [u, v]: mp[it]) {
+            f[u][v][1] = f[v][u][1] = it;
+            for (int i = 1; i <= n; i++) {
+                if (i == v || i == u) continue;
+                bool c1 = false, c2 = false;
+                for (int k = m; k >= 0; k--) {
+                    if (f[u][i][k] != 2e9) {
+                        c1 = true;
+                    }
+                    if (f[v][i][k] != 2e9) {
+                        c2 = true;
+                    }
+                    if (c1) {
+                        f[v][i][k + 1] = f[i][v][k + 1] = it;
+                    }
+                    if (c2) {
+                        f[u][i][k + 1] = f[i][u][k + 1] = it;
+                    }
+                }
+            }
+        }
+        // for (int k = 1; k <= m; k++) {
+        //     for (int i = 1; i <= n; i++) {
+        //         for (int j = 1; j <= n; j++) {
+        //             cout << f[i][j][k] << ' ';
+        //         }
+        //         cout << endl;
+        //     }
+        //     cout << endl;
+        // }
+    }
+    while (q--) {
+        int a, b, k;
+        cin >> a >> b >> k;
+        cout << f[a][b][k] << ' ';
+    }
+    cout << endl;
 }
 
 int main() {
