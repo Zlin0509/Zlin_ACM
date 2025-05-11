44cf0e46322b1002e895355806b04a80fc4d8da3
zlin <2768228421@qq.com>
2025-05-11 15:14:12 +0800

    daily

--- _CF/CONTEST/2000-2499/2028/E.cpp
+++ _CF/CONTEST/2000-2499/2028/E.cpp
@@ -13,16 +13,75 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-typedef pair<int,int> pii;
-typedef pair<long long,long long>pll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+constexpr int N = 2e5 + 10;
+constexpr ll mo = 998244353;
+
+inline ll qpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % mo;
+        a = a * a % mo;
+        b >>= 1;
+    }
+    return res;
+}
+
+int n, in[N], f[N];
+vi e[N];
+ll val[N];
+
+inline void dfs(int u, int fa) {
+    f[u] = fa;
+    for (int v: e[u]) {
+        if (v == fa) continue;
+        ++in[u];
+        dfs(v, u);
+    }
+}
 
 inline void Zlin() {
-
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        val[i] = 0;
+        in[i] = 0;
+        e[i].clear();
+    }
+    for (int i = 1, u, v; i < n; i++) {
+        cin >> u >> v;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+    dfs(1, 0);
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if (!in[i]) {
+            val[i] = 1;
+            q.push(i);
+        }
+    }
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        int v = f[u];
+        val[v] = max(val[v], val[u] * 2);
+        if (!--in[v]) q.push(v);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) {
+            cout << 1 << ' ';
+        } else {
+            cout << (1 - qpow(val[i], mo - 2) + mo) % mo << ' ';
+        }
+    }
+    cout << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
