6a1bb2c0512de6ac6b8c2a1ddfd568f7cd12dd02
Zlin_PC <zlin0509@outlok.com>
2025-05-21 18:00:55 +0800

    daily

--- _CF/CONTEST/1500-1999/1693/C.cpp
+++ _CF/CONTEST/1500-1999/1693/C.cpp
@@ -16,14 +16,46 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+constexpr int N = 2e5 + 10;
+
+int n, m, dis[N], vis[N], in[N];
+vi e[N];
+
+
 inline void Zlin() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        vis[i] = 0;
+        dis[i] = 2e9;
+    }
+    for (int i = 1, u, v; i <= m; i++) {
+        cin >> u >> v;
+        e[v].push_back(u);
+        ++in[u];
+    }
+    dis[n] = 0;
+    priority_queue<pii> pq;
+    pq.push({-dis[n], n});
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+        if (vis[u]) continue;
+        vis[u] = 1;
+        for (int v: e[u]) {
+            int w = in[v]--;
+            if (!vis[v] && dis[u] + w < dis[v]) {
+                dis[v] = dis[u] + w;
+                pq.push({-dis[v], v});
+            }
+        }
+    }
+    cout << dis[1] << endl;
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
-    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
