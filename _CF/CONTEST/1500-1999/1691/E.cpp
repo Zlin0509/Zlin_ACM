9ad7f1c5721aee584d9cc0667bd0675f9f958039
Zlin_PC <zlin0509@outlok.com>
2025-05-21 19:08:11 +0800

    daily

--- _CF/CONTEST/1500-1999/1691/E.cpp
+++ _CF/CONTEST/1500-1999/1691/E.cpp
@@ -16,13 +16,25 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-inline void Zlin() {
 
+constexpr int N = 1e5 + 10;
+
+int n;
+vector<pii> line[2];
+
+inline void Zlin() {
+    line[0].clear(), line[1].clear();
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        int c, l, r;
+        cin >> c >> l >> r;
+        line[c].push_back({l, r});
+    }
 }
 
 signed main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr),cout.tie(nullptr);
+    cin.tie(nullptr), cout.tie(nullptr);
     int ttt = 1;
     cin >> ttt;
     while (ttt--) Zlin();
