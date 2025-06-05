052b797380e5efc6c1ff1019e561aa80bdc3f15b
Zlin_PC <zlin0509@outlok.com>
2025-06-05 17:28:00 +0800

    daily

--- _CF/CONTEST/1500-1999/1607/F.cpp
+++ _CF/CONTEST/1500-1999/1607/F.cpp
@@ -5,56 +5,56 @@
 #include "bits/stdc++.h"
 #define endl '\n'
 using namespace std;
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-typedef __int128 i128;
-typedef double db;
-typedef long double ldb;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<int> vi;
-typedef vector<long long> vll;
 typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
 
-constexpr int N = 2001;
+constexpr int N = 4e6 + 5;
 
-int dx[4]{0, 0, 1, -1}, dy[4]{1, -1, 0, 0};
+char c;
+int n, m, tmp;
+int nxt[N], val[N];
+int vis[N]{2};
 
-int n, m;
-int mp[N][N], val[N][N], vis[N][N];
+inline int dfs(int u) {
+    if (vis[u] == 2) return val[u];
+    if (vis[u] == 1) {
+        int siz = 1;
+        for (int it = nxt[u]; it != u; it = nxt[it]) ++siz;
+        for (int it = nxt[u]; it != u; it = nxt[it]) val[it] = siz, vis[it] = 2;
+        val[u] = siz, vis[u] = 2;
+        return 0;
+    }
+    vis[u] = 1;
+    dfs(nxt[u]);
+    if (vis[u] != 2) {
+        val[u] = val[nxt[u]] + 1;
+        vis[u] = 2;
+    }
+    return val[u];
+}
 
+int be = 0, en = 0;
 
 inline void Zlin() {
     cin >> n >> m;
+    be = en + 1;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            char c;
+            ++en;
             cin >> c;
-            if (c == 'R') mp[i][j] = 0;
-            if (c == 'L') mp[i][j] = 1;
-            if (c == 'D') mp[i][j] = 2;
-            if (c == 'U') mp[i][j] = 3;
-            val[i][j] = vis[i][j] = 0;
+            if (c == 'L' && j != 1) nxt[en] = en - 1;
+            if (c == 'R' && j != m) nxt[en] = en + 1;
+            if (c == 'D' && i != n) nxt[en] = en + m;
+            if (c == 'U' && i != 1) nxt[en] = en - m;
         }
     }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (!vis[i][j]) {
-                vector<pii> stk;
-            }
+    int ans = 0, pos = 0;
+    for (int i = be; i <= en; i++) {
+        if (ans < dfs(i)) {
+            ans = val[i];
+            pos = i;
         }
     }
-    int ans = 0;
-    pii pos;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (ans < val[i][j]) {
-                pos = {i, j};
-                ans = val[i][j];
-            }
-        }
-    }
-    cout << pos.first << " " << pos.second << ' ' << ans << endl;
+    cout << (pos - be) / m + 1 << ' ' << (pos - be) % m + 1 << ' ' << ans << endl;
 }
 
 signed main() {
