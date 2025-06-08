ddb98cef19ebcf49a190169c860f7beb6af732e6
zzz <zlin0509@>
2025-06-09 00:47:06 +0800

    daily

--- _CF/CONTEST/2000-2499/2117/H.cpp
+++ _CF/CONTEST/2000-2499/2117/H.cpp
@@ -5,18 +5,49 @@
 #define endl '\n'
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
 typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int getk(vi &a) {
+    int n = a.size(), res = 0;
+    map<int, int> mp;
+    for (int it: a) mp[it]++;
+    vector<pii> top(mp.begin(), mp.end());
+    sort(top.begin(), top.end(), [](auto &x, auto &y) { return x.second > y.second; });
+    for (int i = 0; i < min(5, (int) top.size()); i++) {
+        int v = top[i].first;
+        vi pre(n + 1);
+        for (int j = 0; j < n; j++) pre[j + 1] = pre[j] + (a[j] == v);
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < min(n, l + 1000); ++r) {
+                int len = r - l + 1;
+                int cnt = pre[r + 1] - pre[l];
+                int need = (len + 1) / 2;
+                if (cnt >= need) res = max(res, cnt - need);
+            }
+        }
+    }
+    return res;
+}
 
 inline void Zlin() {
-
+    int n, q;
+    cin >> n >> q;
+    vi a(n);
+    for (int &it: a) cin >> it;
+    while (q--) {
+        int i, x;
+        cin >> i >> x;
+        a[i - 1] = x;
+        cout << getk(a) << ' ';
+    }
+    cout << endl;
 }
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    int ttt = 1;
-    cin >> ttt;
-    while (ttt--) Zlin();
+    int t;
+    cin >> t;
+    while (t--) Zlin();
     return 0;
 }
