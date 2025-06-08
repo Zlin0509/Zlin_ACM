90c5efed13489b0c247d2cafbd76a74effb0729a
Zlin_PC <zlin0509@outlok.com>
2025-06-08 17:13:08 +0800

    daily

--- _Game/A.cpp
+++ _Game/A.cpp
@@ -5,52 +5,14 @@ using namespace std;
 typedef long long ll;
 
 constexpr ll mo = 1000000007;
-constexpr int N = 1e6 + 10;
 
-ll n, val[N], k, inv;
-vector<int> e[N];
-
-inline ll qpow(ll a, ll b) {
-    ll res = 1;
-    while (b) {
-        if (b & 1) res = res * a % mo;
-        a = a * a % mo;
-        b >>= 1;
-    }
-    return res;
-}
-
-inline void dfs(int u, int fa) {
-    ll all = 1, cnt = 0;
-    for (int v: e[u]) {
-        if (v == fa) continue;
-        dfs(v, u);
-        all = val[v] * inv % mo * all % mo;
-        ++cnt;
-    }
-    if (!cnt) {
-        val[u] = k;
-    } else {
-        ll tmp = k;
-        for (int i = 1; i <= min(cnt, k); i++) {
-            tmp = tmp * (k - i) % mo;
-            ll res = all * qpow(i, cnt - i) % mo * tmp % mo;
-            val[u] = (val[u] + res) % mo;
-        }
-    }
-}
+int n, k;
 
 inline void Zlin() {
     cin >> n >> k;
-    inv = qpow(k, mo - 2);
-    for (int i = 1; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        e[u].push_back(v);
-        e[v].push_back(u);
-    }
-    dfs(1, 0);
-    cout << val[1] << endl;
+    ll ans = k;
+    for (int i = 1; i < n; i++) ans = ans * (k - 1) % mo;
+    cout << ans << endl;
 }
 
 signed main() {
