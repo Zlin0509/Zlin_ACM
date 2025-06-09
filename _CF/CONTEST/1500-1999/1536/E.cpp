9fe2edc3c3dc33fcded391fa8b1d11183565b8b5
zlin <2768228421@qq.com>
2025-06-09 09:06:12 +0800

    daily

--- _CF/CONTEST/1500-1999/1536/E.cpp
+++ _CF/CONTEST/1500-1999/1536/E.cpp
@@ -16,7 +16,25 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+constexpr ll mo = 1e9 + 7;
+constexpr int N = 2010;
+
+int dx[4]{1, -1, 0, 0}, dy[4]{0, 0, 1, -1};
+
+int n, m;
+ll val[N][N];
+char mp[N][N];
+
 inline void Zlin() {
+    cin >> n >> m;
+    queue<pii> q;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> mp[i][j];
+            val[i][j] = 1;
+            if (mp[i][j] == '0') q.emplace(i, j);
+        }
+    }
 }
 
 signed main() {
