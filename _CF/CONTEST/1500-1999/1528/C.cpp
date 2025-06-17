b948eb14df1c3e09dba37e4158764d744c2bf5a8
zzz <zlin0509@>
2025-06-17 21:42:13 +0800

    daily

---
+++ _CF/CONTEST/1500-1999/1528/C.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 27682 on 2025/6/17.
+//
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+inline void Zlin() {
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int ttt = 1;
+    cin >> ttt;
+    while (ttt--) Zlin();
+    return 0;
+}
