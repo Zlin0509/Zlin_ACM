147ebcf6c24765fdebfcd723f5353e99e953875b
zlin_pc <21369432@qq.com>
2025-06-22 00:53:19 +0800

    daily

--- _CF/CONTEST/1000-1499/1495/C.cpp
+++ _CF/CONTEST/1000-1499/1495/C.cpp
@@ -14,7 +14,32 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+int n, m;
+char mp[501][501];
+
+vi idx[501];
+
 inline void Zlin() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        idx[i].clear();
+        for (int j = 1; j <= m; j++) {
+            cin >> mp[i][j];
+            if (mp[i][j] == 'X') idx[i].push_back(j);
+        }
+    }
+    for (int i = 1; i <= n; i += 3) {
+        for (int j = 1; j <= m; j++) mp[i][j] = 'X';
+        if (i + 3 <= n) {
+            if (!idx[i + 2].empty()) mp[i + 1][idx[i + 2][0]] = 'X';
+            else if (!idx[i + 1].empty()) mp[i + 2][idx[i + 1][0]] = 'X';
+            else mp[i + 1][1] = mp[i + 2][1] = 'X';
+        } else if (i + 2 == n) for (int j: idx[i + 2]) mp[i + 1][j] = 'X';
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) cout << mp[i][j];
+        cout << endl;
+    }
 }
 
 signed main() {
