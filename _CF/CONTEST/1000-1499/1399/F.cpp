94dca219f62bd12013d90a95a665228a70a0f9d1
zlin_pc <21369432@qq.com>
2025-06-25 23:04:44 +0800

    daily

--- _CF/CONTEST/1000-1499/1399/F.cpp
+++ _CF/CONTEST/1000-1499/1399/F.cpp
@@ -5,7 +5,29 @@
 #define endl '\n'
 using namespace std;
 
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+constexpr int N = 3e3 + 10;
+
+int n;
+pii line[N];
+
 inline void Zlin() {
+    cin >> n;
+    vi idx;
+    for (int i = 1; i <= n; i++) {
+        cin >> line[i].first >> line[i].second;
+        idx.push_back(line[i].first);
+        idx.push_back(line[i].second);
+    }
+    idx.push_back(0), idx.push_back(2e5 + 1);
+    sort(idx.begin(), idx.end());
+    idx.erase(unique(idx.begin(), idx.end()), idx.end());
+    for (int i = 1; i <= n; i++) {
+        line[i].first = lower_bound(idx.begin(), idx.end(), line[i].first) - idx.begin();
+        line[i].second = lower_bound(idx.begin(), idx.end(), line[i].second) - idx.begin();
+    }
 }
 
 signed main() {
