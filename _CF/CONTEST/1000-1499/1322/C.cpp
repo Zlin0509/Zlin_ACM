62b05cc9b745ee251298c28c8949e2a9c0ab52a7
zzz <zlin0509@>
2025-06-25 16:55:57 +0800

    daily

--- _CF/CONTEST/1000-1499/1322/C.cpp
+++ _CF/CONTEST/1000-1499/1322/C.cpp
@@ -4,7 +4,7 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
-
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef long long ll;
 typedef vector<ll> vll;
 
@@ -13,13 +13,18 @@ constexpr int N = 5e5 + 10;
 inline void Zlin() {
     int n, m;
     cin >> n >> m;
-    vll c(n), val(n);
-    for (int i = 0; i < n; i++) cin >> c[i];
+    vll c(n), val(n), idx(n);
+    map<ll, ll> mp;
+    for (int i = 0; i < n; i++) cin >> c[i], idx[i] = rng();
     for (int i = 1, u, v; i <= m; i++) {
         cin >> u >> v;
         --u, --v;
-        val[u] += c[v];
+        val[v] += idx[u];
     }
+    for (int i = 0; i < n; i++) mp[val[i]] += c[i];
+    ll ans = 0;
+    for (auto &[k, v]: mp) if (k) ans = ans ? gcd(ans, v) : v;
+    cout << ans << endl;
 }
 
 signed main() {
