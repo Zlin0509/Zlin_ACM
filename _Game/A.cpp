4d385f6f9c01a625060e85ed3e6354950c08f3ad
zzz <zlin0509@>
2025-06-29 12:04:43 +0800

    daily

--- _Game/A.cpp
+++ _Game/A.cpp
@@ -3,15 +3,42 @@
 using namespace std;
 
 typedef long long ll;
+typedef vector<int> vi;
 
-constexpr ll mo = 1000000007;
+constexpr int N = 3e5 + 10;
 
-int n, k;
+int n, m, k, lx[N], rx[N], val[N * 3], used[N];
+vi have, in[N * 3], out[N * 3];
 
 inline void Zlin() {
     cin >> n >> k;
-    ll ans = k;
-    for (int i = 1; i < n; i++) ans = ans * (k - 1) % mo;
+    have.clear();
+    for (int i = 1; i <= n; i++) {
+        used[i] = 0;
+        cin >> lx[i] >> rx[i];
+        have.push_back(lx[i]);
+        have.push_back(rx[i]);
+    }
+    sort(have.begin(), have.end());
+    have.erase(unique(have.begin(), have.end()), have.end());
+    m = have.size();
+    for (int i = 0; i < m; i++) {
+        val[i] = 0;
+        in[i].clear();
+        out[i].clear();
+    }
+    for (int i = 1; i <= n; i++) {
+        lx[i] = lower_bound(have.begin(), have.end(), lx[i]) - have.begin();
+        rx[i] = lower_bound(have.begin(), have.end(), rx[i]) - have.begin();
+        in[rx[i]].push_back(i);
+        if (lx[i]) out[lx[i] - 1].push_back(i);
+    }
+    int ans = 0;
+    for (int i = m - 1, cnt = 0; ~i; i--) {
+        cnt += in[i].size() + out[i].size();
+        val[i] = cnt;
+        ans = max(ans, cnt);
+    }
     cout << ans << endl;
 }
 
@@ -19,6 +46,7 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int ttt = 1;
+    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
