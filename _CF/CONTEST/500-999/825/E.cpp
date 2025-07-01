272fb362c99525fd4874518cb488e0ddd46bbdbc
zlin_pc <21369432@qq.com>
2025-07-02 00:07:07 +0800

    daily

--- _CF/CONTEST/500-999/825/E.cpp
+++ _CF/CONTEST/500-999/825/E.cpp
@@ -9,19 +9,28 @@ typedef vector<int> vi;
 constexpr int N = 1e5 + 10;
 
 vi e[N];
-int n, m, in[N], ans[N], tot = 0;
+int n, m, in[N], ans[N];
 
 inline void Zlin() {
     cin >> n >> m;
     for (int i = 1, u, v; i <= m; i++) {
         cin >> u >> v;
-        ++in[v];
-        e[u].push_back(v);
+        ++in[u];
+        e[v].push_back(u);
     }
-
-
-
-
+    priority_queue<int> pq;
+    int tot = n;
+    for (int i = 1; i <= n; i++) if (!in[i]) pq.push(i);
+    while (!pq.empty()) {
+        int u = pq.top();
+        pq.pop();
+        ans[u] = tot--;
+        for (int v: e[u]) {
+            if (!--in[v]) {
+                pq.push(v);
+            }
+        }
+    }
     for (int i = 1; i <= n; i++) cout << ans[i] << ' ';
 }
 
