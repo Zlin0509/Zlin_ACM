329b33c976b9f82c8dcd283065d033717b5229fb
zzz <zlin0509@>
2025-07-01 20:01:54 +0800

    daily

--- _CF/CONTEST/500-999/825/E.cpp
+++ _CF/CONTEST/500-999/825/E.cpp
@@ -4,7 +4,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef vector<int> vi;
+
+constexpr int N = 1e5 + 10;
+
+vi e[N];
+int n, m, in[N], ans[N], tot = 0;
+
 inline void Zlin() {
+    cin >> n >> m;
+    for (int i = 1, u, v; i <= m; i++) {
+        cin >> u >> v;
+        ++in[v];
+        e[u].push_back(v);
+    }
+
+
+
+
+    for (int i = 1; i <= n; i++) cout << ans[i] << ' ';
 }
 
 signed main() {
