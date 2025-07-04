b0d527a119a883aef235f60ef20daf71c7398b7c
zlin <2768228421@qq.com>
2025-07-04 16:59:20 +0800

    daily

--- _CF/CONTEST/2000-2499/2023/C.cpp
+++ _CF/CONTEST/2000-2499/2023/C.cpp
@@ -16,14 +16,54 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+constexpr int N = 2e5 + 10;
+
+int n, a[N];
+vi v[N];
+
+bool is_prime[N];
+vi primes;
+
+inline void dd() {
+    memset(is_prime, true, sizeof(is_prime));
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i < N; i++) {
+        if (is_prime[i]) primes.push_back(i);
+        for (int j: primes) {
+            if (i * j > N) break;
+            is_prime[i * j] = false;
+            if (i % j == 0) break;
+        }
+    }
+}
+
 inline void Zlin() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        for (int it: primes) {
+            int cnt = 0;
+            while (a[i] % it == 0) {
+                ++cnt;
+                a[i] /= it;
+            }
+            v[it].emplace_back(cnt);
+        }
+    }
+    ll ans = 1;
+    for (int it: primes) {
+        sort(v[it].begin(), v[it].end());
+        int tag = v[it][1];
+        while (tag--) ans *= it;
+    }
+    cout << ans << endl;
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
+    dd();
     int ttt = 1;
-    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
