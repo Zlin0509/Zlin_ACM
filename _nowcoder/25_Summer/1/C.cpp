13b5324e5edc8b837ba201afb9790854e65def2b
zzz <zlin0509@>
2025-07-16 12:34:46 +0800

    daily

--- _nowcoder/25_Summer/1/C.cpp
+++ _nowcoder/25_Summer/1/C.cpp
@@ -5,30 +5,33 @@
 using namespace std;
 
 typedef long long ll;
+typedef vector<int> vi;
+
+constexpr int N = 1010;
 
 int n, q, d;
-ll a[1010], sum[1010], w[1010], ans[1010];
+ll a[N], w[N];
+
+struct Seg {
+    struct node {
+        int l, r;
+        ll a, b;
+    } t[N << 2];
+
+    void build(int i, int l, int r) {
+        t[i].l = l, t[i].r = r;
+    }
+} t;
+
+vi have;
 
 inline void Zlin() {
     cin >> n >> q;
-    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) cin >> a[i], have.emplace_back(a[i]);
     for (int i = 1; i < n; i++) cin >> w[i];
-    for (int i = 1; i < n; i++) sum[i] = sum[i - 1] + abs(a[i] - a[i + 1]) * w[i];
-    for (int l = 1; l <= n; l++) {
-        for (int r = l; r <= n; r++) {
-            ll res = sum[r - 1] - sum[l - 1];
-            if (r != 1) res += w[l - 1] * abs(l - a[l]);
-            if (r != n) res += w[r] * abs(r - a[r]);
-            ans[r - l] = max(ans[r - l], res);
-            if (r - l == 2) {
-                cout << res << endl;
-            }
-        }
-    }
-    while (q--) {
-        cin >> d;
-        cout << ans[d] << endl;
-    }
+    sort(have.begin(), have.end());
+    have.erase(unique(have.begin(), have.end()), have.end());
+    t.build(1, 0, have.size() - 1);
 }
 
 signed main() {
