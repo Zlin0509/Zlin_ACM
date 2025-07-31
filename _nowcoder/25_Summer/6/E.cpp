8735ab0dfaaffddcde225c1c1cbcdb85c4957b78
zzz <zlin0509@>
2025-07-31 19:05:43 +0800

    daily

--- _nowcoder/25_Summer/6/E.cpp
+++ _nowcoder/25_Summer/6/E.cpp
@@ -15,91 +15,102 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 constexpr int N = 1e5 + 10;
-constexpr ll INF = 1e18;
 
-struct Dij_Tree {
-    struct Node {
-        int l, r;
-    } t[N << 2];
-
-    struct edge {
-        int to;
-        ll val;
+inline void Zlin() {
+    int n, q;
+    cin >> n >> q;
+    vi p(n);
+    for (int &it: p) {
+        cin >> it;
+        --it;
+    }
+    int siz1 = 310, siz2 = (n + siz1 - 1) / siz1;
+    vector cnt(siz2, vi(siz2));
+    // 第一维对应p 第二维对应a
+    for (int i = 0; i < siz2; i++) {
+        int L = i * siz1, R = min(n, L + siz1);
+        for (int j = L; j < R; j++) ++cnt[i][p[j] / siz1];
+    }
+    // 前缀和处理 用来统计对于每一块a p中一段区间内包含多少个
+    vector pre(siz2 + 1, vi(siz2));
+    for (int i = 0; i < siz2; i++) {
+        for (int j = 0; j < siz2; j++) {
+            pre[i + 1][j] = pre[i][j] + cnt[i][j];
+        }
+    }
+    vector<vector<pii> > occ(n);
+    for (int i = 0; i < siz2; i++) {
+        int L = i * siz1, R = min(n, L + siz1);
+        vi val;
+        for (int j = L; j < R; j++) val.emplace_back(p[j]);
+        sort(val.begin(), val.end());
+        for (int l = 0, r; l < val.size(); l = r) {
+            r = l;
+            while (r < val.size() && val[r] <= val[l]) ++r;
+            occ[val[l]].emplace_back(i, r - l);
+        }
+    }
+    vll a(n, 0), tag(siz2, 0), sum(siz2, 0);
+    auto add1 = [&](int i, ll x) {
+        a[i] += x;
+        for (auto [idx, c]: occ[i]) sum[idx] += x * c;
     };
-
-    vector<edge> e[N << 4];
-
-    int dif = 5e5;
-    int idx[N], vis[N << 4];
-    ll dis[N << 4];
-
-
-    void build(int i, int l, int r) {
-        // cout << i << ' ' << l << ' ' << r << endl;
-        dis[i] = dis[i + dif] = INF;
-        vis[i] = vis[i + dif] = 0;
-        t[i].l = l, t[i].r = r;
-        if (l == r) {
-            // 底边互相连接
-            e[i].push_back({i + dif, 0});
-            e[i + dif].push_back({i, 0});
-            idx[l] = i;
-            return;
+    auto add2 = [&](int l, int r, ll x) {
+        int bl = l / siz1, br = r / siz1;
+        if (bl == br) for (int i = l; i <= r; i++) add1(i, x);
+        else {
+            int tmp = (bl + 1) * siz1 - 1;
+            for (int i = l; i <= tmp; i++) add1(i, x);
+            tmp = br * siz1;
+            for (int i = tmp; i <= r; i++) add1(i, x);
+            for (int i = bl + 1; i <= br - 1; i++) tag[i] += x;
         }
-        // 第一个对应坐标
-        e[i].push_back({i << 1, 0});
-        e[i].push_back({i << 1 | 1, 0});
-        // 第二个对应坐标
-        e[(i << 1) + dif].push_back({i + dif, 0});
-        e[(i << 1 | 1) + dif].push_back({i + dif, 0});
-
-        int mid = l + r >> 1;
-        build(i << 1, l, mid);
-        build(i << 1 | 1, mid + 1, r);
-    }
-
-    // op 1 表示从点到线段 点到线段说明是第二棵树的点s 连接 第一棵树的对应node
-    // op 0 表示从线段到点 线段到点说明是第二棵树的对应node 连接 第一棵树的点s
-    void update(int i, int l, int r, int s, int val, int op) {
-        if (t[i].r < l || t[i].l > r) return;
-        if (t[i].l >= l && t[i].r <= r) {
-            if (op) e[idx[s] + dif].push_back({i, val});
-            else e[i + dif].push_back({idx[s], val});
-            return;
-        }
-        update(i << 1, l, r, s, val, op);
-        update(i << 1 | 1, l, r, s, val, op);
-    }
-
-    void dij(int s) {
-        priority_queue<pair<ll, int>, vector<pair<ll, int> >, greater<> > pq;
-        dis[idx[s]] = 0;
-        pq.emplace(dis[idx[s]], idx[s]);
-        while (!pq.empty()) {
-            int u = pq.top().second;
-            pq.pop();
-            if (vis[u]) continue;
-            vis[u] = 1;
-            // cout << (u >= dif ? u - dif : u) << ' ' << dis[u] << endl;
-            for (auto [v, val]: e[u]) {
-                if (dis[v] > dis[u] + val) {
-                    dis[v] = dis[u] + val;
-                    pq.emplace(dis[v], v);
+    };
+    auto get1 = [&](int i) {
+        return a[i] + tag[i / siz1];
+    };
+    auto get2 = [&](int l, int r) {
+        ll ans = 0;
+        int bl = l / siz1, br = r / siz1;
+        if (bl == br) for (int i = l; i <= r; i++) ans += get1(p[i]);
+        else {
+            int tmp = (bl + 1) * siz1 - 1;
+            for (int i = l; i <= tmp; i++) ans += get1(p[i]);
+            tmp = br * siz1;
+            for (int i = tmp; i <= r; i++) ans += get1(p[i]);
+            ++bl, --br;
+            if (bl <= br) {
+                for (int i = bl; i <= br; i++) ans += sum[i];
+                for (int j = 0; j < siz2; j++) {
+                    int k = pre[br + 1][j] - pre[bl][j];
+                    ans += 1ll * k * tag[j];
                 }
             }
         }
+        return ans;
+    };
+    ll ans = 0;
+    while (q--) {
+        int opt;
+        cin >> opt;
+        if (opt == 1) {
+            ll l, r, x;
+            cin >> l >> r >> x;
+            l = (l ^ ans) - 1, r = (r ^ ans) - 1, x ^= ans;
+            add2(l, r, x);
+        } else {
+            ll l, r;
+            cin >> l >> r;
+            l = (l ^ ans) - 1, r = (r ^ ans) - 1;
+            ans = get2(l, r);
+            cout << ans << endl;
+        }
     }
-
-    ll query(int i) { return dis[idx[i]]; }
-} t;
-
-inline void Zlin() {
 }
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int ttt = 1;
-    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
