fa76bc2c579cabd80660f971725b3dc8f1938753
zzz <zlin0509@>
2025-08-09 15:33:05 +0800

    daily

--- _HDU/25 Summer/7/1001.cpp
+++ _HDU/25 Summer/7/1001.cpp
@@ -1,64 +1,108 @@
-#include <bits/stdc++.h>
+//
+// Created by 27682 on 2025/8/9.
+//
+
+#include "bits/stdc++.h"
+#define endl '\n'
 using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-int n, k;
-int p[10005 * 10005];
+typedef double db;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 
-struct node {
-    int x, y, val;
-} val[10005];
+struct Seg {
+    static constexpr int N = 5e4;
 
-int x, y, len1, len2, x2, y2, nx, ny, lx, rx;
+    struct Node {
+        int l, r;
+        int val, tag;
+    } t[N];
 
-void solve() {
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++) cin >> val[i].x >> val[i].y >> val[i].val;
-    for (int i = 1, pos; i <= n; i++) {
-        pos = val[i].x * n + val[i].y;
-        if (p[pos] > 0) p[pos] = 0;
-        p[pos] -= val[i].val;
+    void pushdown(int i) {
+        if (t[i].l != t[i].r) {
+            t[i << 1].val += t[i].tag;
+            t[i << 1].tag += t[i].tag;
+            t[i << 1 | 1].val += t[i].tag;
+            t[i << 1 | 1].tag += t[i].tag;
+        }
+        t[i].tag = 0;
     }
-    for (int i = 1, pos; i <= n; i++) {
-        x = 0;
-        for (int j = 1; j <= n; j++) {
-            pos = i * n + j;
-            if (p[pos] < 0) x -= p[pos];
-            p[pos] = p[pos - n] + x;
+
+    void update(int i) {
+        t[i].val = max(t[i << 1].val, t[i << 1 | 1].val);
+    }
+
+    void build(int i, int l, int r) {
+        t[i] = {l, r, 0, 0};
+        if (l == r) return;
+        int mid = l + r >> 1;
+        build(i << 1, l, mid);
+        build(i << 1 | 1, mid + 1, r);
+    }
+
+    void modify(int i, int l, int r, int val) {
+        if (t[i].r < l || t[i].l > r) return;
+        if (t[i].l >= l && t[i].r <= r) {
+            t[i].val += val;
+            t[i].tag += val;
+            return;
+        }
+        pushdown(i);
+        modify(i << 1, l, r, val);
+        modify(i << 1 | 1, l, r, val);
+        update(i);
+    }
+
+    int query() {
+        return t[1].val;
+    }
+} seg;
+
+constexpr int N = 1e4 + 10;
+
+int n, k, ans;
+array<int, 3> a[N];
+vector<pii> idx[N];
+
+inline int cal(int h, int w) {
+    int res = 0;
+    seg.build(1, 1, n);
+    for (int i = 1; i <= min(h, n); i++) {
+        for (auto [pos, val]: idx[i]) {
+            seg.modify(1, max(1, pos - w + 1), pos, val);
         }
     }
-    int ans = 0;
-    for (int l = 1, r; l <= k;) {
-        r = k / (k / l);
-        for (int i = 1; i <= n; i++) {
-            x = val[i].x, y = val[i].y;
-            len1 = r, len2 = k / r;
-            if (len1 < len2) {
-                lx = max(0, len1 - x), rx = min(len1, n - x + 1);
-                for (int j = lx; j < rx; j++) {
-                    x2 = x + j, y2 = y;
-                    nx = x2 - len1 + 1, ny = max(1, y2 - len2 + 1);
-                    ans = max(
-                        ans, p[x2 * n + y2] - p[x2 * n + ny - 1] - p[(nx - 1) * n + y2] + p[(nx - 1) * n + ny - 1]);
-                }
-            } else {
-                lx = max(0, len2 - y), rx = min(len2, n - y + 1);
-                for (int j = lx; j < rx; j++) {
-                    x2 = x, y2 = y + j;
-                    nx = max(1, x2 - len1 + 1), ny = y2 - len2 + 1;
-                    ans = max(
-                        ans, p[x2 * n + y2] - p[x2 * n + ny - 1] - p[(nx - 1) * n + y2] + p[(nx - 1) * n + ny - 1]);
-            }
-        }
-        l = r + 1;
+    res = max(res, seg.query());
+    for (int i = h + 1, j; i <= n; i++) {
+        j = i - h;
+        for (auto [pos, val]: idx[j]) seg.modify(1, max(1, pos - w + 1), pos, -val);
+        for (auto [pos, val]: idx[i]) seg.modify(1, max(1, pos - w + 1), pos, val);
+        res = max(res, seg.query());
     }
-    cout << ans << "\n";
+    return res;
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int _ = 1;
-    cin >> _;
-    while (_--) solve();
+inline void Zlin() {
+    cin >> n >> k;
+    ans = 0;
+    for (int i = 0; i < n; i++) cin >> a[i][0] >> a[i][1] >> a[i][2];
+    for (int i = 1; i <= n; i++) idx[i].clear();
+    for (int i = 0; i < n; i++) idx[a[i][0]].emplace_back(a[i][1], a[i][2]);
+    for (int i = 1; i * i <= k; i++) ans = max(ans, cal(i, k / i));
+    for (int i = 1; i <= n; i++) idx[i].clear();
+    for (int i = 0; i < n; i++) idx[a[i][1]].emplace_back(a[i][0], a[i][2]);
+    for (int i = 1; i * i <= k; i++) ans = max(ans, cal(i, k / i));
+    cout << ans << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int ttt = 1;
+    cin >> ttt;
+    while (ttt--) Zlin();
     return 0;
 }
