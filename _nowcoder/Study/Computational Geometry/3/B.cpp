1b3c9a6588cee55745c3dd530a91126b33a0b8f0
zlin <2768228421@qq.com>
2025-09-12 11:15:20 +0800

    daily

--- _nowcoder/Study/Computational Geometry/3/B.cpp
+++ _nowcoder/Study/Computational Geometry/3/B.cpp
@@ -14,7 +14,92 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+constexpr db eps = 1e-9;
+const db pi = acos(-1);
+
+template<typename T>
+struct point {
+    T x, y, val;
+
+    bool operator==(const point &a) const { return (abs(x - a.x) <= eps && abs(y - a.y) <= eps); }
+    point operator+(const point &a) const { return {x + a.x, y + a.y}; }
+    point operator-(const point &a) const { return {x - a.x, y - a.y}; }
+    point operator-() const { return {-x, -y}; }
+    point operator*(const T k) const { return {k * x, k * y}; }
+    point operator/(const T k) const { return {x / k, y / k}; }
+    T operator*(const point &a) const { return x * a.x + y * a.y; } // Dot
+    T operator^(const point &a) const { return x * a.y - y * a.x; } // Cross
+    bool operator<(const point &a) const {
+        if (abs(x - a.x) <= eps) return y < a.y - eps;
+        return x < a.x - eps;
+    }
+
+    bool is_par(const point &a) const { return abs((*this) ^ a) <= eps; } // 平行
+    bool is_ver(const point &a) const { return abs((*this) * a) <= eps; } // 垂直
+
+    int toleft(const point &a) const {
+        auto t = (*this) ^ a;
+        return (t > eps) - (t < -eps);
+    }
+
+
+    T len2() const { return (*this) * (*this); }
+    T dis2(const point &a) const { return (a - (*this)).len2(); }
+    double len() const { return sqrt(len2()); }
+    double dis(const point &a) const { return (a - (*this)).len(); }
+    double ang(const point &a) const { return acos(((*this) * a) / (this->len() * a.len())); } // 普通夹角（只返回大小 [0, π]）
+    double signed_ang(const point &a) const { return atan2((*this) ^ a, (*this) * a); } // 带方向夹角：逆时针为正，顺时针为负，范围 (-π, π]
+    point rot(const double rad) const { return {x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad)}; }
+};
+
+template<typename T>
+struct segment {
+    point<T> a, b;
+};
+
+using Point = point<ll>;
+using Seg = segment<ll>;
+
+bool is_on(const Point p, const Point a, const Point b) {
+    return abs((p - a) ^ (p - b)) <= eps && (p - a) * (p - b) <= eps;
+}
+
+bool argcmp(const Point &a, const Point &b) {
+    auto quad = [](const Point &a) {
+        if (a.y < -eps) return 1;
+        if (a.y > eps) return 4;
+        if (a.x > eps) return 3;
+        if (a.x < -eps) return 5;
+        return 2;
+    };
+    int x = quad(a), y = quad(b);
+    if (x != y) return x < y;
+    auto res = a ^ b;
+    if (abs(res) < eps) return a.val > b.val;
+    return res > eps;
+}
+
+
+int n;
+vector<Seg> a;
+
 inline void Zlin() {
+    cin >> n;
+    for (int i = 0, x1, x2, y; i < n; i++) {
+        cin >> x1 >> x2 >> y;
+        a.emplace_back(Point(x1, y), Point(x2, y));
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        Point s = a[i].a;
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            auto [x, y] = a[j];
+            if (x.y < s.y) {
+            }
+        }
+    }
+    cout << ans << endl;
 }
 
 signed main() {
