d54088293e194cc848bbe90afbfc0bcf9c0d781d
zlin <2768228421@qq.com>
2025-09-22 19:41:11 +0800

    daily

--- _nowcoder/Study/Computational Geometry/4.1/E.cpp
+++ _nowcoder/Study/Computational Geometry/4.1/E.cpp
@@ -14,13 +14,231 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-inline void Zlin() {
+using T = long double; //全局数据类型
+
+constexpr T eps = 1e-8;
+constexpr T INF = numeric_limits<T>::max();
+constexpr T PI = 3.1415926535897932384l;
+
+// 点与向量
+struct Point {
+    T x, y;
+
+    bool operator==(const Point &a) const { return (abs(x - a.x) <= eps && abs(y - a.y) <= eps); }
+
+    bool operator<(const Point &a) const {
+        if (abs(x - a.x) <= eps) return y < a.y - eps;
+        return x < a.x - eps;
+    }
+
+    bool operator>(const Point &a) const { return !(*this < a || *this == a); }
+    Point operator+(const Point &a) const { return {x + a.x, y + a.y}; }
+    Point operator-(const Point &a) const { return {x - a.x, y - a.y}; }
+    Point operator-() const { return {-x, -y}; }
+    Point operator*(const T k) const { return {k * x, k * y}; }
+    Point operator/(const T k) const { return {x / k, y / k}; }
+    T operator*(const Point &a) const { return x * a.x + y * a.y; } // 点积
+    T operator^(const Point &a) const { return x * a.y - y * a.x; } // 叉积，注意优先级
+    int toleft(const Point &a) const {
+        const auto t = (*this) ^ a;
+        return (t > eps) - (t < -eps);
+    } // to-left 测试
+    T len2() const { return (*this) * (*this); } // 向量长度的平方
+    T dis2(const Point &a) const { return (a - (*this)).len2(); } // 两点距离的平方
+    int quad() const // 象限判断 0:原点 1:x轴正 2:第一象限 3:y轴正 4:第二象限 5:x轴负 6:第三象限 7:y轴负 8:第四象限
+    {
+        if (abs(x) <= eps && abs(y) <= eps) return 0;
+        if (abs(y) <= eps) return x > eps ? 1 : 5;
+        if (abs(x) <= eps) return y > eps ? 3 : 7;
+        return y > eps ? (x > eps ? 2 : 4) : (x > eps ? 8 : 6);
+    }
+
+    // 必须用浮点数
+    T len() const { return sqrtl(len2()); } // 向量长度
+    T dis(const Point &a) const { return sqrtl(dis2(a)); } // 两点距离
+    T ang(const Point &a) const { return acosl(max(-1.0l, min(1.0l, ((*this) * a) / (len() * a.len())))); } // 向量夹角
+    Point rot(const T rad) const { return {x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad)}; } // 逆时针旋转（给定角度）
+    Point rot(const T cosr, const T sinr) const { return {x * cosr - y * sinr, x * sinr + y * cosr}; }
+    // 逆时针旋转（给定角度的正弦与余弦）
+};
+
+// 极角排序
+struct Argcmp {
+    bool operator()(const Point &a, const Point &b) const {
+        const int qa = a.quad(), qb = b.quad();
+        if (qa != qb) return qa < qb;
+        const auto t = a ^ b;
+        // if (abs(t)<=eps) return a*a<b*b-eps;  // 不同长度的向量需要分开
+        return t > eps;
+    }
+};
+
+// 直线
+struct Line {
+    Point p, v; // p 为直线上一点，v 为方向向量
+
+    bool operator==(const Line &a) const { return v.toleft(a.v) == 0 && v.toleft(p - a.p) == 0; }
+    int toleft(const Point &a) const { return v.toleft(a - p); } // to-left 测试
+    bool operator<(const Line &a) const // 半平面交算法定义的排序
+    {
+        if (abs(v ^ a.v) <= eps && v * a.v >= -eps) return toleft(a.p) == -1;
+        return Argcmp()(v, a.v);
+    }
+
+    // 必须用浮点数
+    Point inter(const Line &a) const { return p + v * ((a.v ^ (p - a.p)) / (v ^ a.v)); } // 直线交点
+    T dis(const Point &a) const { return abs(v ^ (a - p)) / v.len(); } // 点到直线距离
+    Point proj(const Point &a) const { return p + v * ((v * (a - p)) / (v * v)); } // 点在直线上的投影
+};
+
+//线段
+struct Segment {
+    Point a, b;
+
+    bool operator<(const Segment &s) const { return make_pair(a, b) < make_pair(s.a, s.b); }
+
+    // 判定性函数建议在整数域使用
+
+    // 判断点是否在线段上
+    // -1 点在线段端点 | 0 点不在线段上 | 1 点严格在线段上
+    int is_on(const Point &p) const {
+        if (p == a || p == b) return -1;
+        return (p - a).toleft(p - b) == 0 && (p - a) * (p - b) < -eps;
+    }
+
+    // 判断线段直线是否相交
+    // -1 直线经过线段端点 | 0 线段和直线不相交 | 1 线段和直线严格相交
+    int is_inter(const Line &l) const {
+        if (l.toleft(a) == 0 || l.toleft(b) == 0) return -1;
+        return l.toleft(a) != l.toleft(b);
+    }
+
+    // 判断两线段是否相交
+    // -1 在某一线段端点处相交 | 0 两线段不相交 | 1 两线段严格相交
+    int is_inter(const Segment &s) const {
+        if (is_on(s.a) || is_on(s.b) || s.is_on(a) || s.is_on(b)) return -1;
+        const Line l{a, b - a}, ls{s.a, s.b - s.a};
+        return l.toleft(s.a) * l.toleft(s.b) == -1 && ls.toleft(a) * ls.toleft(b) == -1;
+    }
+
+    // 点到线段距离（必须用浮点数）
+    T dis(const Point &p) const {
+        if ((p - a) * (b - a) < -eps || (p - b) * (a - b) < -eps) return min(p.dis(a), p.dis(b));
+        const Line l{a, b - a};
+        return l.dis(p);
+    }
+
+    // 两线段间距离（必须用浮点数）
+    T dis(const Segment &s) const {
+        if (is_inter(s)) return 0;
+        return min({dis(s.a), dis(s.b), s.dis(a), s.dis(b)});
+    }
+};
+
+// 多边形
+struct Polygon {
+    vector<Point> p; // 以逆时针顺序存储
+
+    size_t nxt(const size_t i) const { return i == p.size() - 1 ? 0 : i + 1; }
+    size_t pre(const size_t i) const { return i == 0 ? p.size() - 1 : i - 1; }
+
+    // 回转数
+    // 返回值第一项表示点是否在多边形边上
+    // 对于狭义多边形，回转数为 0 表示点在多边形外，否则点在多边形内
+    pair<bool, int> winding(const Point &a) const {
+        int cnt = 0;
+        for (size_t i = 0; i < p.size(); i++) {
+            const Point u = p[i], v = p[nxt(i)];
+            if (abs((a - u) ^ (a - v)) <= eps && (a - u) * (a - v) <= eps) return {true, 0};
+            if (abs(u.y - v.y) <= eps) continue;
+            const Line uv = {u, v - u};
+            if (u.y < v.y - eps && uv.toleft(a) <= 0) continue;
+            if (u.y > v.y + eps && uv.toleft(a) >= 0) continue;
+            if (u.y < a.y - eps && v.y >= a.y - eps) cnt++;
+            if (u.y >= a.y - eps && v.y < a.y - eps) cnt--;
+        }
+        return {false, cnt};
+    }
+
+    // 多边形面积的两倍
+    // 可用于判断点的存储顺序是顺时针或逆时针
+    T area() const {
+        T sum = 0;
+        for (size_t i = 0; i < p.size(); i++) sum += p[i] ^ p[nxt(i)];
+        return sum;
+    }
+
+    // 多边形的周长
+    T circ() const {
+        T sum = 0;
+        for (size_t i = 0; i < p.size(); i++) sum += p[i].dis(p[nxt(i)]);
+        return sum;
+    }
+};
+
+inline T p2line(const Point &u, const Point &v, const Point &w) {
+    Line l(u, v - u);
+    return l.dis(w);
+}
+
+//凸多边形
+struct Convex : Polygon {
+    T rotcaliper() const {
+        T ans = INF;
+        const auto &p = this->p;
+        const auto area = [](const Point &u, const Point &v, const Point &w) { return (u - w) ^ (v - w); };
+        for (size_t i = 0, j = 1; i < p.size(); i++) {
+            size_t nxti = this->nxt(i);
+            while (area(p[this->nxt(j)], p[i], p[nxti]) >= area(p[j], p[i], p[nxti])) {
+                j = this->nxt(j);
+            }
+            ans = min(ans, p2line(p[i], p[nxti], p[j]));
+        }
+        return ans;
+    }
+};
+
+Convex convexhull(vector<Point> p) {
+    vector<Point> res;
+    if (p.empty()) return {res};
+    sort(p.begin(), p.end());
+    const auto check = [](const vector<Point> &res, const Point &u) {
+        const auto b1 = res.back(), b2 = *prev(res.end(), 2);
+        return (b1 - b2).toleft(u - b1) <= 0;
+    };
+    for (const auto &u: p) {
+        while (res.size() > 1 && check(res, u)) res.pop_back();
+        res.emplace_back(u);
+    }
+    int siz = res.size();
+    res.pop_back();
+    reverse(p.begin(), p.end());
+    for (const auto &u: p) {
+        while (res.size() > siz && check(res, u)) res.pop_back();
+        res.emplace_back(u);
+    }
+    res.pop_back();
+    return {res};
+}
+
+inline void Zlin(int n) {
+    vector<Point> p;
+    for (int i = 0; i < n; i++) {
+        T x, y;
+        cin >> x >> y;
+        p.emplace_back(x, y);
+    }
+    Convex poly = convexhull(p);
+    cout << fixed << setprecision(10) << poly.rotcaliper() << endl;
 }
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    int ttt = 1;
-    cin >> ttt;
-    while (ttt--) Zlin();
+    while (1) {
+        int n;
+        cin >> n;
+        if (!n) break;
+        Zlin(n);
+    }
     return 0;
 }
