6759630b127102bb34ff0ed8e3f390ba330c4e55
27682 <123@123.123>
2025-10-13 15:46:55 +0800

    update

--- _CF/CONTEST/2000-2499/2118/E.cpp
+++ _CF/CONTEST/2000-2499/2118/E.cpp
@@ -14,7 +14,58 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+
 inline void Zlin() {
+    int n, m;
+    cin >> n >> m;
+    vector<pii> ans;
+    auto check = [&](const int &x, const int &y) {
+        return 1 <= x && x <= n && 1 <= y && y <= m;
+    };
+    const int sx = n / 2 + 1, sy = m / 2 + 1;
+    ans.emplace_back(sx, sy);
+    for (int i = 1, x, y; i <= max(n / 2, m / 2); i++) {
+        x = sx - i, y = sy;
+        if (check(x, y)) ans.emplace_back(x, y);
+        x = sx + i, y = sy;
+        if (check(x, y)) ans.emplace_back(x, y);
+        for (int j = 1; j < i; j++) {
+            x = sx - i, y = sy + j;
+            if (check(x, y)) ans.emplace_back(x, y);
+            x = sx + i, y = sy + j;
+            if (check(x, y)) ans.emplace_back(x, y);
+            x = sx - i, y = sy - j;
+            if (check(x, y)) ans.emplace_back(x, y);
+            x = sx + i, y = sy - j;
+            if (check(x, y)) ans.emplace_back(x, y);
+        }
+
+        x = sx, y = sy - i;
+        if (check(x, y)) ans.emplace_back(x, y);
+        x = sx, y = sy + i;
+        if (check(x, y)) ans.emplace_back(x, y);
+        for (int j = 1; j < i; j++) {
+            x = sx + j, y = sy - i;
+            if (check(x, y)) ans.emplace_back(x, y);
+            x = sx + j, y = sy + i;
+            if (check(x, y)) ans.emplace_back(x, y);
+            x = sx - j, y = sy - i;
+            if (check(x, y)) ans.emplace_back(x, y);
+            x = sx - j, y = sy + i;
+            if (check(x, y)) ans.emplace_back(x, y);
+        }
+
+        x = sx - i, y = sy - i;
+        if (check(x, y)) ans.emplace_back(x, y);
+        x = sx + i, y = sy + i;
+        if (check(x, y)) ans.emplace_back(x, y);
+        x = sx - i, y = sy + i;
+        if (check(x, y)) ans.emplace_back(x, y);
+        x = sx + i, y = sy - i;
+        if (check(x, y)) ans.emplace_back(x, y);
+    }
+    for (const auto &[x, y]: ans)cout << x << ' ' << y << endl;
+    cout << endl;
 }
 
 signed main() {
