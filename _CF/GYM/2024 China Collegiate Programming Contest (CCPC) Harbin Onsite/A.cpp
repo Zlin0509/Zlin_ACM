dccde3dd14ffd052c6cad39d1b91659fd22a2e90
zzz <zlin0509@>
2025-10-28 19:45:40 +0800

    R9000P

--- _CF/GYM/2024 China Collegiate Programming Contest (CCPC) Harbin Onsite/A.cpp
+++ _CF/GYM/2024 China Collegiate Programming Contest (CCPC) Harbin Onsite/A.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Zlin on 2025/8/3.
+// Created by 27682 on 2025/10/28.
 //
 
 #include "bits/stdc++.h"
@@ -14,94 +14,13 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-constexpr int N = 1e6 + 10;
-
-int l, r, cnt = 0;
-int base = 0, nxt[N], mx = 1;
-vector<pii> ans[N];
-map<pii, int> dfn;
-int dfn1[N];
-
-inline bool dfs(int base, int dep) {
-    if (dep < 0) return base >= l && base <= r;
-    if (base > r || base + (1 << dep + 1) - 1 < l) return false;
-    dfn[{base, dep}] = ++cnt;
-    if (base + (1 << dep + 1) - 1 <= r && base >= l) {
-        mx = max(mx, dep + 1);
-        nxt[dfn[{base, dep}]] = 6 + dep;
-        return true;
-    }
-    bool check = false;
-    if (dfs(base + (1 << dep), dep - 1)) {
-        if (dep) nxt[dfn[{base, dep}]] |= 1;
-        check = true;
-    }
-    if (dfs(base, dep - 1)) {
-        if (dep) nxt[dfn[{base, dep}]] |= 2;
-        check = true;
-    }
-    return check;
-}
-
-inline void work(int base, int dep) {
-    if (dep < 0) return;
-    int p = dfn[{base, dep}], lp = dfn[{base, dep - 1}], rp = dfn[{base + (1 << dep), dep - 1}];
-    if (nxt[p] > 5) {
-        ans[p].emplace_back(cnt + nxt[p] - 5, 0);
-        ans[p].emplace_back(cnt + nxt[p] - 5, 1);
-    } else if (nxt[p] == 3) {
-        ans[p].emplace_back(lp, 0);
-        work(base, dep - 1);
-        ans[p].emplace_back(rp, 1);
-        work(base + (1 << dep), dep - 1);
-    } else if (nxt[p] == 2) {
-        ans[p].emplace_back(lp, 0);
-        work(base, dep - 1);
-    } else if (nxt[p] == 1) {
-        ans[p].emplace_back(rp, 1);
-        work(base + (1 << dep), dep - 1);
-    } else if (dep == 0) {
-        if (base >= l && base <= r) ans[p].emplace_back(cnt + 1, 0);
-        if (base + 1 >= l && base + 1 <= r) ans[p].emplace_back(cnt + 1, 1);
-    }
-}
-
-vector<pii> g[N];
-int cc = 0;
-
-inline void dfs1(int u, int tag) {
-    if (dfn1[u]) return;
-    for (auto [v, val]: ans[u]) tag |= val;
-    if (tag && !dfn1[u]) dfn1[u] = ++cc;
-    for (auto [v, val]: ans[u]) {
-        dfs1(v, tag);
-        if (tag) g[dfn1[u]].emplace_back(dfn1[v], val);
-    }
-}
-
 inline void Zlin() {
-    cin >> l >> r;
-    cnt = 0, mx = 1;
-    dfs(0, 21);
-    work(0, 21);
-    for (int i = cnt + mx; i > cnt + 1; i--) {
-        ans[i].emplace_back(i - 1, 0);
-        ans[i].emplace_back(i - 1, 1);
-    }
-    dfs1(1, 0);
-    cout << cc << endl;
-    for (int i = 1; i <= cc; i++) {
-        cout << g[i].size() << ' ';
-        for (auto [x, y]: g[i]) cout << x << ' ' << y << ' ';
-        cout << endl;
-    }
-    if (cc > 100) while (1);
 }
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int ttt = 1;
-    // ttt = 10000;
+    cin >> ttt;
     while (ttt--) Zlin();
     return 0;
 }
--- _CF/GYM/The 2024 ICPC Asia Shenyang Regional Contest/G.cpp
+++ _CF/GYM/The 2024 ICPC Asia Shenyang Regional Contest/G.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+typedef long double ldb;
 typedef double db;
 typedef long long ll;
 typedef vector<int> vi;
@@ -13,160 +14,64 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-struct Fraction {
-    long long num; // 分子
-    long long den; // 分母
-
-    // 构造函数
-    Fraction(long long _num = 0, long long _den = 1) {
-        if (_den == 0) throw runtime_error("Denominator cannot be zero");
-        if (_den < 0) _num = -_num, _den = -_den; // 保持分母为正
-        long long g = gcd(abs(_num), abs(_den));
-        num = _num / g;
-        den = _den / g;
-    }
-
-    // 约分
-    void reduce() {
-        if (den < 0) num = -num, den = -den;
-        long long g = gcd(abs(num), abs(den));
-        num /= g;
-        den /= g;
-    }
-
-    // 加法
-    Fraction operator+(const Fraction &other) const {
-        return Fraction(num * other.den + other.num * den, den * other.den);
-    }
-
-    // 减法
-    Fraction operator-(const Fraction &other) const {
-        return Fraction(num * other.den - other.num * den, den * other.den);
-    }
-
-    // 乘法
-    Fraction operator*(const Fraction &other) const {
-        return Fraction(num * other.num, den * other.den);
-    }
-
-    // 除法
-    Fraction operator/(const Fraction &other) const {
-        if (other.num == 0) throw runtime_error("Division by zero fraction");
-        return Fraction(num * other.den, den * other.num);
-    }
-
-    // 复合赋值运算符
-    Fraction &operator+=(const Fraction &other) { return *this = *this + other; }
-    Fraction &operator-=(const Fraction &other) { return *this = *this - other; }
-    Fraction &operator*=(const Fraction &other) { return *this = *this * other; }
-    Fraction &operator/=(const Fraction &other) { return *this = *this / other; }
-
-    // 比较运算
-    bool operator==(const Fraction &other) const {
-        return num == other.num && den == other.den;
-    }
-
-    bool operator!=(const Fraction &other) const {
-        return !(*this == other);
-    }
-
-    bool operator<(const Fraction &other) const {
-        return num * other.den < other.num * den;
-    }
-
-    bool operator>(const Fraction &other) const {
-        return num * other.den > other.num * den;
-    }
-
-    bool operator<=(const Fraction &other) const {
-        return !(*this > other);
-    }
-
-    bool operator>=(const Fraction &other) const {
-        return !(*this < other);
-    }
-
-    // 输出
-    friend ostream &operator<<(ostream &os, const Fraction &f) {
-        if (f.den == 1) return os << f.num;
-        return os << f.num << "/" << f.den;
-    }
-
-    // 输入
-    friend istream &operator>>(istream &is, Fraction &f) {
-        string s;
-        is >> s;
-        auto pos = s.find('/');
-        if (pos == string::npos) {
-            f = Fraction(stoll(s), 1);
-        } else {
-            long long n = stoll(s.substr(0, pos));
-            long long d = stoll(s.substr(pos + 1));
-            f = Fraction(n, d);
-        }
-        return is;
-    }
-} k(1, 4);
-
 constexpr int N = 1005;
+constexpr ll mo = 998244353;
 
-int n, cc[N];
-pair<Fraction, Fraction> res[N];
-vi a;
+inline ll qpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % mo;
+        a = a * a % mo;
+        b >>= 1;
+    }
+    return res;
+}
+
+inline ll re(string s) {
+    ll res = 0;
+    for (const char &it: s) res = (res * 10 + it - '0') % mo;
+    return res;
+}
+
+inline ll ask(ll u, ll v) {
+    cout << "? " << u << ' ' << v << endl;
+    string x, y;
+    cin >> x >> y;
+    u = re(x), v = re(y);
+    return u * qpow(v, mo - 2) % mo;
+}
+
+inline void output(ll ans) {
+    ans = ans * 2 % mo;
+    if (ans & 1) cout << "! " << ans << " " << 2 << endl;
+    else cout << "! " << ans / 2 << " " << 1 << endl;
+}
 
 inline void Zlin() {
+    int n;
+    vi tag;
     cin >> n;
-    a.clear();
-    memset(cc, 0, sizeof(cc));
-    for (int i = 1, x, y; i <= n; i++) {
+    for (int i = 0, x, y; i < n; i++) {
         cin >> x >> y;
-        ++cc[x];
-        a.emplace_back(x);
+        tag.emplace_back(x);
     }
-    sort(a.begin(), a.end());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    for (int i = 0; i < a.size(); i++) {
-        if (cc[a[i]] == 1) {
-            if (i > 0 && i < a.size() - 1) {
-                cout << "? " << a[i] << " 1" << endl;
-                cin >> res[i].first.num >> res[i].first.den;
-                res[i].second = res[i].first;
-            } else res[i].first = res[i].second = Fraction(0, 1);
-        } else {
-            Fraction xx = {a[i], 1};
-            xx -= k;
-            cout << "? " << xx.num << " " << xx.den << endl;
-            cin >> res[i].first.num >> res[i].first.den;
-            xx += k, xx += k;
-            cout << "? " << xx.num << " " << xx.den << endl;
-            cin >> res[i].second.num >> res[i].second.den;
+    sort(tag.begin(), tag.end());
+    tag.erase(unique(tag.begin(), tag.end()), tag.end());
+    ll ans = 0;
+    if (tag.size() != n) {
+        for (int i = 1; i < tag.size(); i++)
+            ans += 1ll * (tag[i] - tag[i - 1]) * ask(tag[i - 1] + tag[i], 2) % mo;
+        output(ans % mo);
+    } else {
+        ll pre = 0, now;
+        for (int i = 1; i < tag.size(); i++) {
+            if (i == tag.size() - 1) now = 0;
+            else now = ask(tag[i], 1);
+            ans += 1ll * (tag[i] - tag[i - 1]) * (pre + now) % mo;
+            pre = now;
         }
+        output(ans % mo * qpow(2, mo - 2) % mo);
     }
-    Fraction ans{0, 1};
-    for (int i = 1; i < a.size(); i++) {
-        Fraction dif = k;
-        dif *= {(cc[a[i - 1]] > 1) + (cc[a[i]] > 1), 1};
-        dif.reduce();
-        dif = Fraction(a[i] - a[i - 1], 1) - dif;
-        dif.reduce();
-        dif = (res[i].first - res[i - 1].second) / dif;
-        dif.reduce();
-
-        Fraction tmp = {0, 1};
-        if (cc[a[i]] > 1) tmp += res[i].first + dif * k;
-        else tmp += res[i].first;
-        tmp.reduce();
-        if (cc[a[i - 1]] > 1) tmp += res[i - 1].second - dif * k;
-        else tmp += res[i - 1].second;
-        tmp.reduce();
-
-        tmp *= Fraction(a[i] - a[i - 1], 2);
-        tmp.reduce();
-
-        ans += tmp;
-        ans.reduce();
-    }
-    cout << "! " << ans.num << " " << ans.den << endl;
 }
 
 signed main() {
