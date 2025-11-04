7a35e0a3444b3116a548ac87e673be9d0b0ff1c5
zzz <zlin0509@>
2025-11-04 14:34:25 +0800

    R9000P

--- _CF/CONTEST/2000-2499/2154/E.cpp
+++ _CF/CONTEST/2000-2499/2154/E.cpp
@@ -14,7 +14,37 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+constexpr int N = 2e5 + 10;
+
+int n, k, a[N];
+ll sum[N];
+map<int, int> cnt;
+
+inline ll cal(int i, int j) {
+    ll res = sum[n] - sum[i + j] + 1ll * a[i] * (j + 1);
+    int x = min((ll) i - 1, 1ll * j * k);
+    res += sum[i - 1] - sum[x] + 1ll * x * a[i];
+    return res;
+}
+
 inline void Zlin() {
+    cin >> n >> k;
+    cnt.clear();
+    for (int i = 1; i <= n; i++) cin >> a[i], cnt[a[i]]++;
+    sort(a + 1, a + 1 + n);
+    for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + a[i];
+    ll ans = sum[n];
+    for (int i = 1; i <= n; i++) {
+        int l = 0, r = n - i;
+        while (l + 2 < r) {
+            int ml = l + (r - l) / 3, mr = r - (r - l) / 3;
+            ll vl = cal(i, ml), vr = cal(i, mr);
+            if (vl > vr) r = mr;
+            else l = ml;
+        }
+        for (int j = l; j <= r; j++) ans = max(ans, cal(i, j));
+    }
+    cout << ans << endl;
 }
 
 signed main() {
