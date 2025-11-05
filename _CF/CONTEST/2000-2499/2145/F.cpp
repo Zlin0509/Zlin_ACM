5c34786df2bf92b90e929b795e9ce22f299d93a9
zzz <zlin0509@>
2025-11-05 18:18:28 +0800

    2145F

--- _CF/CONTEST/2000-2499/2145/F.cpp
+++ _CF/CONTEST/2000-2499/2145/F.cpp
@@ -14,23 +14,41 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-constexpr ll INF = 1e16;
 constexpr int N = 2520;
 
 int n, a[10], b[10];
-ll m, dp[N][N];
-
-inline void dfs(int x, int t, int s) {
-    if (x % a[(t + s) % n] == b[(t + s) % n]) return;
-    if (x == 10) dp[s][(s + t) % n] = min(dp[s][(s + t) % n], (ll) t);
-    dfs(x, t + 1, s);
-    dfs(x + 1, t + 1, s);
-}
+ll m, dp[10][N][50];
 
 inline void Zlin() {
     cin >> n >> m;
     for (int i = 0; i < n; i++) cin >> a[i];
     for (int i = 0; i < n; i++) cin >> b[i];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < N; j++) {
+            if ((j + 1) % a[i] != b[i]) dp[i][j][0] = 1;
+            else dp[i][j][0] = 0;
+        }
+    }
+    for (int k = 1; k < 50; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < N; j++) {
+                dp[i][j][k] = dp[i][j][k - 1] + dp[((1ll << (k - 1)) + i) % n][(dp[i][j][k - 1] + j) % N][k - 1];
+            }
+        }
+    }
+    if (dp[0][0][49] < m) {
+        cout << -1 << endl;
+        return;
+    }
+    ll ans = 0, now = 0;
+    for (int i = 49; ~i; i--) {
+        if (now + dp[ans % n][now % N][i] < m) {
+            now += dp[ans % n][now % N][i];
+            ans += 1ll << i;
+            // cout << ans << ' ' << now << endl;
+        }
+    }
+    cout << ans + 1 << endl;
 }
 
 signed main() {
